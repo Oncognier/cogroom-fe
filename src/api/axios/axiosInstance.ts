aa9a39ae303dc0dbf8a51eb4520372b6873a8f25
jihun <gfsd2397@naver.com>
2025-05-17 21:16:28 +0900

    feat: axiosInstance 설정 #6 (#7)
    feat: axiosInstance 생성 & 요청 및 응답 interceptor
    
    feat: axios config useAuth 추가 및 interceptor 설정
    
    chore: axios 쿠키 설정
    
    refactor: axios interceptor 내부함수 분리
    
    chore: interceptor 응답 시 에러 구조화
    
    feat: 응답 성공 handler
    
    feat: axios interceptor request & response handler
    
    feat: axios interceptor request & response handler
    
    refactor: utils HTTPError 폴더명 변경 및 interceptor error throw로 변경
    
    feat: delete config headers authorization
    
    refactor: gitkeep 파일 제거
    
    refactor: _axiosInstance to axios

---
+++ src/api/axios/axiosInstance.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { requestHandler, requestErrorHandler, responseHandler, responseErrorHandler } from '@/api/axios/interceptors';
+
+const axiosInstance = axios.create({
+  // 기본 URL 설정
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+
+  // 요청 제한 시간: 10초
+  timeout: 10000,
+
+  // 쿠키 설정
+  withCredentials: true,
+
+  // 로그인 여부
+  useAuth: true,
+});
+
+// 요청 시 interceptor
+axiosInstance.interceptors.request.use(requestHandler, requestErrorHandler);
+
+// 응답 시 interceptor
+axiosInstance.interceptors.response.use(responseHandler, responseErrorHandler);
+
+export default axiosInstance;
