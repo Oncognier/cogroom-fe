e453672477c16dd47b5d80d233987a68d1a782aa
jihun <gfsd2397@naver.com>
2025-05-19 17:18:04 +0900

    chore: msw 설정 #8 (#11)
    chore: init msw
    
    chore: mocks server & client
    
    feat: msw 연동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,23 @@
 import EmotionRegistry from './EmotionRegistry';
+import { MSWProvider } from './MSWProvider';
+
+// MSW 서버 적용
+if (
+  process.env.NEXT_RUNTIME === 'nodejs' &&
+  process.env.NODE_ENV !== 'production' &&
+  process.env.NEXT_PUBLIC_MSW_ENABLED !== 'false'
+) {
+  const { server } = require('@/mocks/server');
+  server.listen();
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
       <body>
-        <EmotionRegistry>{children}</EmotionRegistry>
+        <EmotionRegistry>
+          <MSWProvider>{children}</MSWProvider>
+        </EmotionRegistry>
       </body>
     </html>
   );
