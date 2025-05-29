54f96a7c3f3ff75a266e6f31354cc75af9b1dcc3
Seio924 <seio9241@gmail.com>
2025-05-29 19:06:26 +0900

    feat: 기본 Thumbnail 상수로 정의

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,1 +1,2 @@
 export const DEFAULT_AVATAR_IMAGE = 'https://d3k85mwe6suqlp.cloudfront.net/default_image/Profile.png' as const;
+export const DEFAULT_THUMBNAIL = 'https://d3k85mwe6suqlp.cloudfront.net/default_image/Thumbnail.png' as const;
