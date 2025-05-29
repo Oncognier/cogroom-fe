99d61caeb843f00253466712b2937c59f6cc72dc
Seio924 <seio9241@gmail.com>
2025-05-30 00:20:39 +0900

    feat: ToggleIcon 컴포넌트 구현

---
+++ src/components/atoms/ToggleIcon/ToggleIcon.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import S, { ToggleIconStyleProps } from './ToggleIcon.styled';
+
+interface ToggleIconProps extends ToggleIconStyleProps {
+  onToggle: (checked: boolean) => void;
+  children: React.ReactNode;
+}
+
+export default function ToggleIcon({ size, isActive, onToggle, interactionVariant, children }: ToggleIconProps) {
+  const handleClick = () => {
+    onToggle(!isActive);
+  };
+
+  return (
+    <S.ToggleIcon
+      size={size}
+      isActive={isActive}
+      interactionVariant={interactionVariant}
+      onClick={handleClick}
+    >
+      {children}
+    </S.ToggleIcon>
+  );
+}
