30740a433cc22fc825bfd981189faf062c5dab9e
Seio924 <seio9241@gmail.com>
2025-05-31 16:57:18 +0900

    fix: pretendard 폰트 적용 문제 해결

--- .storybook/globalStylesStorybook.tsx
+++ .storybook/globalStylesStorybook.tsx
@@ -1,8 +1,6 @@
 import { css } from '@emotion/react';
 
 const globalStylesStorybook = () => css`
-  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css');
-
   /* Reset CSS */
   * {
     margin: 0;
