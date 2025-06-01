6c7a66994e9edc259553d25420ce0b3e37f2def0
Seio924 <seio9241@gmail.com>
2025-06-01 10:33:17 +0900

    feat: Input 컴포넌트 구현

---
+++ src/components/molcules/Input/Input.stories.tsx
@@ -0,0 +1,83 @@
+import { action } from '@storybook/addon-actions';
+import type { Meta, StoryObj } from '@storybook/react';
+import { useEffect, useState } from 'react';
+
+import Input from './Input';
+
+const meta = {
+  title: 'components/atoms/Input',
+  component: Input,
+  tags: ['autodocs'],
+  argTypes: {
+    inputSize: {
+      control: 'radio',
+      options: ['sm', 'md', 'lg'],
+    },
+    label: {
+      control: 'text',
+    },
+    value: {
+      control: 'text',
+    },
+    required: {
+      control: 'boolean',
+    },
+    isDisabled: {
+      control: 'boolean',
+    },
+    errorMessage: {
+      control: 'text',
+    },
+    error: {
+      control: 'text',
+    },
+    errorStatus: {
+      control: 'radio',
+      options: ['error', 'warning', 'info', 'success'],
+    },
+    onChange: { action: 'changed' },
+    onClear: { action: 'cleared' },
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    inputSize: 'md',
+    label: '이메일',
+    value: '',
+    required: true,
+    isDisabled: false,
+    errorMessage: '',
+    error: '',
+    errorStatus: 'error',
+  },
+  render: (args) => {
+    const [inputValue, setInputValue] = useState(args.value);
+
+    useEffect(() => {
+      setInputValue(args.value);
+    }, [args.value]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(e.target.value);
+      action('changed')(e.target.value);
+    };
+
+    const handleClear = () => {
+      setInputValue('');
+      action('cleared')();
+    };
+
+    return (
+      <Input
+        {...args}
+        value={inputValue}
+        onChange={handleChange}
+        onClear={handleClear}
+      />
+    );
+  },
+};
---
+++ src/components/molcules/Input/Input.tsx
@@ -0,0 +1,76 @@
+import { ComponentProps } from 'react';
+
+import XCircle from '@/assets/icons/xcircle-fill.svg';
+import FormStatusMessage from '@/components/atoms/FormStatusMessage/FormStatusMessage';
+import { FormStatusMessageStatus } from '@/components/atoms/FormStatusMessage/FormStatusMessage.styled';
+import InputLabel from '@/components/atoms/InputLabel/InputLabel';
+
+import S, { InputStyleProps } from './Input.styled';
+
+interface InputProps extends ComponentProps<'input'>, InputStyleProps {
+  label?: string;
+  errorMessage?: string;
+  errorStatus?: FormStatusMessageStatus;
+  error?: string;
+  isDisabled?: boolean;
+  onClear?: () => void;
+}
+
+export default function Input({
+  label,
+  value,
+  onChange,
+  inputSize,
+  required,
+  isDisabled,
+  errorMessage,
+  errorStatus,
+  error,
+  onClear,
+  ...props
+}: InputProps) {
+  const hasError = !!error || !!errorMessage;
+  const hasValue = Boolean(value);
+
+  return (
+    <S.Container>
+      {label && (
+        <InputLabel
+          label={label}
+          required={required}
+        />
+      )}
+
+      <S.InputWrapper>
+        <S.Input
+          inputSize={inputSize}
+          value={value}
+          onChange={onChange}
+          disabled={isDisabled}
+          isError={hasError}
+          hasValue={hasValue}
+          required={required}
+          {...props}
+        />
+        {hasValue && (
+          <S.RemoveButton
+            type='button'
+            onClick={onClear}
+            aria-label='입력값 지우기'
+          >
+            <XCircle />
+          </S.RemoveButton>
+        )}
+      </S.InputWrapper>
+
+      {errorMessage && <S.Error>{errorMessage}</S.Error>}
+
+      {error && (
+        <FormStatusMessage
+          status={errorStatus}
+          label={error}
+        />
+      )}
+    </S.Container>
+  );
+}
