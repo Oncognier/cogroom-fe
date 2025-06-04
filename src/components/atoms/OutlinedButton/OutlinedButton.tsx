8ec4d78bc3beed56d83668a8f1212d0a5b7367e0
Seio924 <seio9241@gmail.com>
2025-06-04 22:00:16 +0900

    feat: solid, outline에 use client 추가

--- src/components/atoms/OutlinedButton/OutlinedButton.tsx
+++ src/components/atoms/OutlinedButton/OutlinedButton.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import S, { OutlinedButtonStyleProps } from './OutlinedButton.styled';
 
 interface OutlinedButtonProps extends OutlinedButtonStyleProps {
--- src/components/atoms/SolidButton/SolidButton.tsx
+++ src/components/atoms/SolidButton/SolidButton.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import S, { SolidButtonStyleProps } from './SolidButton.styled';
 
 interface SolidButtonProps extends SolidButtonStyleProps {
