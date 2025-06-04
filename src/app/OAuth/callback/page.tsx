9b6fae37e4b1092e70a1c73e5cfb56e18828ec35
Seio924 <seio9241@gmail.com>
2025-06-04 18:14:12 +0900

    feat: OAuthCallback 페이지 구현

--- src/app/OAuth/callback/page.tsx
+++ src/app/OAuth/callback/page.tsx
@@ -1,3 +1,19 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { useLogInMutation } from '@/hooks/api/auth/useLogInMutation';
+
 export default function OAuthCallback() {
-  return <div>Loading..</div>;
+  const searchParams = useSearchParams();
+  const code = searchParams.get('code');
+  const { mutateLogIn } = useLogInMutation();
+
+  useEffect(() => {
+    if (!code) return;
+    mutateLogIn({ provider: 'KAKAO', code });
+  }, [code, mutateLogIn]);
+
+  return <div>Loading...</div>;
 }
