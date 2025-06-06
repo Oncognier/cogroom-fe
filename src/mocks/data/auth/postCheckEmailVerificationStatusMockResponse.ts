b65e6809147495173b8ecbe8601676b5c8efbfbf
Seio924 <seio9241@gmail.com>
2025-06-06 14:26:48 +0900

    refactor: mock data 파일명 변경

--- src/mocks/data/auth/postCheckEmailVerificationStatusMockResponse.ts
+++ src/mocks/data/auth/postCheckEmailVerificationStatusMockResponse.ts
@@ -1,9 +1,9 @@
-export const postCheckEmailVerificationStatusResponse = {
+export const postCheckEmailVerificationStatusMockResponse = {
   code: '1000',
   message: '이메일 인증에 성공했습니다.',
 };
 
-export const postLoginMockErrorResponse = {
+export const postCheckEmailVerificationStatusMockErrorResponse = {
   code: '1001',
   message: '이메일 인증에 실패했습니다.',
 };
