deac13ecafd30f4a874f06f9386bd73c49062a30
Seio924 <seio9241@gmail.com>
2025-06-07 11:55:40 +0900

    feat: Sidebar 사용자 정보 조회 추가

--- src/app/mypage/_components/Sidebar/Sidebar.tsx
+++ src/app/mypage/_components/Sidebar/Sidebar.tsx
@@ -1,32 +1,41 @@
-import AvatarPerson from '@/components/atoms/AvatarPerson/AvatarPerson';
-import SidebarNavItem from './SidebarNavItem/SidebarNavItem';
-import S from './Sidebar.styled';
-import { SIDEBAR_NAV_ITEMS } from '@/constants/common';
 import { usePathname } from 'next/navigation';
 
+import AvatarPerson from '@/components/atoms/AvatarPerson/AvatarPerson';
+import { SIDEBAR_NAV_ITEMS } from '@/constants/common';
+import useGetUserSummary from '@/hooks/api/member/useGetUserSummary';
+
+import S from './Sidebar.styled';
+import SidebarNavItem from './SidebarNavItem/SidebarNavItem';
+
 export default function Sidebar() {
+  const { data, isLoading } = useGetUserSummary();
   const pathname = usePathname() || '/';
 
   return (
     <S.Sidebar>
-      <S.Profile>
-        <AvatarPerson
-          type='icon'
-          size='lg'
-        />
-        <S.UserName>홍길동 님</S.UserName>
-      </S.Profile>
-      <S.SidebarNavList>
-        {SIDEBAR_NAV_ITEMS.map(({ label, href }) => (
-          <SidebarNavItem
-            key={href}
-            label={label}
-            href={href}
-            isActive={pathname.startsWith(href)}
-          />
-        ))}
-        <S.Logout>로그아웃</S.Logout>
-      </S.SidebarNavList>
+      {!isLoading && (
+        <>
+          <S.Profile>
+            <AvatarPerson
+              type='icon'
+              size='lg'
+              src={data?.imageUrl}
+            />
+            <S.UserName>{data?.nickname || 's'}</S.UserName>
+          </S.Profile>
+          <S.SidebarNavList>
+            {SIDEBAR_NAV_ITEMS.map(({ label, href }) => (
+              <SidebarNavItem
+                key={href}
+                label={label}
+                href={href}
+                isActive={pathname.startsWith(href)}
+              />
+            ))}
+            <S.Logout>로그아웃</S.Logout>
+          </S.SidebarNavList>
+        </>
+      )}
     </S.Sidebar>
   );
 }
