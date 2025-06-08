36041aa88d8ed3d0e84e48a51073a1372629c951
jeehun <jeehun@jeehunui-MacBookPro.local>
2025-06-08 17:17:42 +0900

    feat: daily page

--- src/app/daily/page.tsx
+++ src/app/daily/page.tsx
@@ -1,3 +1,48 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+import { getDaily, getStreakCalendar } from '@/app/daily/_api/dailyApis';
+import Calendar from '@/app/daily/_components/calendar/Calendar';
+import Question from '@/app/daily/_components/question/Question';
+import Streak from '@/app/daily/_components/streak/Streak';
+import { DailyQuestionResponse, StreakCalendarResponse } from '@/app/daily/_types/daily';
+import Breadcrumb from '@/components/molecules/Breadcrumb/Breadcrumb';
+
+import * as S from './styled';
+
 export default function Daily() {
-  return <div>daily</div>;
+  const [dailyData, setDailyData] = useState<DailyQuestionResponse | null>(null);
+  const [calendarData, setCalendarData] = useState<StreakCalendarResponse | null>(null);
+
+  const getDailyData = async () => {
+    const data = await getDaily();
+    setDailyData(data);
+  };
+
+  const getCalendarData = async () => {
+    const data = await getStreakCalendar();
+    setCalendarData(data);
+  };
+
+  useEffect(() => {
+    getDailyData();
+    getCalendarData();
+  }, []);
+
+  return (
+    <S.DailyPageWrapper>
+      <Breadcrumb
+        items={[
+          { name: '홈', href: '/' },
+          { name: '데일리', href: '/daily' },
+        ]}
+      />
+      <S.DailyContentWrapper>
+        <Streak streaksDays={dailyData?.result.streakDays ?? 0} />
+        <Question question={dailyData?.result.question ?? ''} answer={dailyData?.result.answer ?? ''}/>
+        <Calendar streakDateList={calendarData?.result.streakDateList ?? []} />
+      </S.DailyContentWrapper>
+    </S.DailyPageWrapper>
+  );
 }
---
+++ src/app/daily/styled.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+// layout
+export const DailyWrapper = styled.main`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Spacer = styled.div`
+  height: 20rem;
+`;
+
+// page
+export const DailyPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+
+  max-width: 1100px;
+  margin: 3.2rem auto;
+  padding: 0px 2rem;
+`;
+
+export const DailyContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+`;
