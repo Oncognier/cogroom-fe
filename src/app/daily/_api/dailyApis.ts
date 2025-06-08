510d090e2633bf021946e44900ebfe9f6e92000c
jeehun <jeehun@jeehunui-MacBookPro.local>
2025-06-08 19:31:34 +0900

    style: api 반환 타입 표시

--- src/app/daily/_api/dailyApis.ts
+++ src/app/daily/_api/dailyApis.ts
@@ -1,14 +1,15 @@
 import type { AxiosResponse } from 'axios';
 
 import { axiosInstance } from '@/api/axios/axiosInstance';
-import { DailyAnswerRequest, DailyQuestionResponse } from '@/app/daily/_types/daily';
+import { DailyAnswerRequest, DailyQuestionResponse, StreakCalendarResponse } from '@/app/daily/_types/daily';
 import { END_POINTS_V1 } from '@/constants/api';
 import { ApiResponse } from '@/types/api';
 
 export const getDaily = async () => {
-  const response = await axiosInstance.get<DailyQuestionResponse>(END_POINTS_V1.DAILY.QUESTIONS, {
-    useAuth: true,
-  });
+  const response = await axiosInstance.get<DailyQuestionResponse, AxiosResponse<ApiResponse>>(
+    END_POINTS_V1.DAILY.QUESTIONS,
+    { useAuth: true },
+  );
   return response.data;
 };
 
@@ -24,15 +25,16 @@ export const postDailyAnswer = async ({ answer }: DailyAnswerRequest) => {
 export const patchDailyAnswer = async ({ answer }: DailyAnswerRequest) => {
   const response = await axiosInstance.patch<DailyAnswerRequest, AxiosResponse<ApiResponse>>(
     END_POINTS_V1.DAILY.ANSWERS,
-    { data: answer },
+    { answer },
     { useAuth: true },
   );
   return response.data;
 };
 
 export const getStreakCalendar = async () => {
-  const response = await axiosInstance.get(END_POINTS_V1.STREAKS.CALENDAR, {
-    useAuth: true,
-  });
+  const response = await axiosInstance.get<StreakCalendarResponse, AxiosResponse<ApiResponse>>(
+    END_POINTS_V1.STREAKS.CALENDAR,
+    { useAuth: true },
+  );
   return response.data;
 };
