d63b254ac2ecd34fc07c1c7589ae925970573a44
jeehun <gfsd2397@gmail.com>
2025-06-11 10:44:17 +0900

    feat: daily api type assignedQuestionId 추가

--- src/types/daily.ts
+++ src/types/daily.ts
@@ -4,11 +4,13 @@ export interface DailyQuestionResponse extends ApiResponse {
   result: {
     streakDays: number;
     questionId: number;
+    assignedQuestionId: number;
     question: string;
     answer?: string;
   };
 }
 
 export interface DailyAnswerRequest {
+  assignedQuestionId: number;
   answer: string;
 }
