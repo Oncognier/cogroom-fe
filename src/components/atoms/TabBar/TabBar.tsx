2cff14753e4ca61414d361e5fd2d6d3b36233e0a
Seio924 <seio9241@gmail.com>
2025-06-12 20:37:40 +0900

    feat: TabBar 컴포넌트 구현

---
+++ src/components/atoms/TabBar/TabBar.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import S, { TabBarStyleProps } from './TabBar.styled';
+
+interface TabBarProps extends TabBarStyleProps {
+  label: string;
+  onChange: () => void;
+}
+
+export default function TabBar({ size, state, fillContainer, label, interactionVariant, onChange }: TabBarProps) {
+  return (
+    <S.TabBar
+      size={size}
+      state={state}
+      fillContainer={fillContainer}
+      interactionVariant={interactionVariant}
+      onClick={onChange}
+    >
+      {label}
+    </S.TabBar>
+  );
+}
