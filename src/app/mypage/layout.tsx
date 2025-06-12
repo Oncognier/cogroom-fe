78d6cd25ac864297a884a40ce1d75d50444609d5
Seio924 <seio9241@gmail.com>
2025-06-12 11:58:19 +0900

    feat: MyPage에 Breadcrumb 추가

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import MyPageBreadcrumb from './_components/MyPageBreadcrumb/MyPageBreadcrumb';
 import Sidebar from './_components/Sidebar/Sidebar';
 import S from './layout.styled';
 
 const MypageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <S.MainLayout>
+      <MyPageBreadcrumb />
       <S.Layout>
         <Sidebar />
         <S.Content>{children}</S.Content>
