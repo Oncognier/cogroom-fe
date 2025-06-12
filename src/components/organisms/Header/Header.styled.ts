90d3048b6080e10d586fd4787ecabb78472f9d8a
오지훈 <gfsd2397@gmail.com>
2025-06-13 01:59:47 +0900

    fix: footer & header 수정

--- src/components/organisms/Header/Header.styled.ts
+++ src/components/organisms/Header/Header.styled.ts
@@ -3,19 +3,20 @@
 import styled from '@emotion/styled';
 
 export const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
   width: 100%;
-  height: 6.8rem;
+  height: 6.9rem;
 
   background-color: ${({ theme }) => theme.semantic.static.white};
   border-bottom: 1px solid ${({ theme }) => theme.semantic.line.normal};
-  backdrop-filter: blur(1.6rem);
-  -webkit-backdrop-filter: blur(1.6rem);
+`;
 
-  padding: 1.3rem 17rem;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    padding: 1.3rem;
-  }
+  height: 100%;
+  max-width: 1100px;
+  padding: 0 2rem;
+  margin: 0 auto;
 `;
