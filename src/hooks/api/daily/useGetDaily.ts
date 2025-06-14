37edcb35cf409d0cdcd374f65c9cb7a9d90d3805
오지훈 <139518173+hunitory@users.noreply.github.com>
2025-06-14 19:27:59 +0900

    [FEAT] daily streaks api enabled #99 (#104)
    * fix: use client 제거
    
    * feat: enabled 추가

--- src/hooks/api/daily/useGetDaily.ts
+++ src/hooks/api/daily/useGetDaily.ts
@@ -1,13 +1,15 @@
-'use client';
-
 import { useQuery } from '@tanstack/react-query';
 
 import { dailyApi } from '@/api/dailyApis';
 import { DAILY_QUERY_KEYS } from '@/constants/queryKeys';
+import { useAuthStore } from '@/stores/useAuthStore';
 
 export default function useGetDailyQuery() {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
   return useQuery({
     queryKey: [...DAILY_QUERY_KEYS.DAILY],
     queryFn: dailyApi.getDaily,
+    enabled: isLoggedIn,
   });
 }
