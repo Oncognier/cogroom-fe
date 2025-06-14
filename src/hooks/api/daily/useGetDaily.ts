d985c2659dc722690a7953b6eea3005530b44649
오지훈 <gfsd2397@gmail.com>
2025-06-14 18:12:25 +0900

    feat: enabled 추가

--- src/hooks/api/daily/useGetDaily.ts
+++ src/hooks/api/daily/useGetDaily.ts
@@ -2,10 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 
 import { dailyApi } from '@/api/dailyApis';
 import { DAILY_QUERY_KEYS } from '@/constants/queryKeys';
+import { useAuthStore } from '@/stores/useAuthStore';
 
 export default function useGetDailyQuery() {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
   return useQuery({
     queryKey: [...DAILY_QUERY_KEYS.DAILY],
     queryFn: dailyApi.getDaily,
+    enabled: isLoggedIn,
   });
 }
--- src/hooks/api/streak/useGetStreakDays.ts
+++ src/hooks/api/streak/useGetStreakDays.ts
@@ -2,10 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 
 import { streakApi } from '@/api/streakApis';
 import { STREAK_QUERY_KEYS } from '@/constants/queryKeys';
+import { useAuthStore } from '@/stores/useAuthStore';
 
 export default function useGetStreakDaysQuery() {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
   return useQuery({
     queryKey: [STREAK_QUERY_KEYS.STREAK_DAYS],
     queryFn: streakApi.getStreakDateList,
+    enabled: isLoggedIn,
   });
 }
