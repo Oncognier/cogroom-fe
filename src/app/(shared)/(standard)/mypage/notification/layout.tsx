eaa0e2a7090bb206aae18a9193eb1c4de834e55b
Seio924 <seio9241@gmail.com>
2025-06-15 15:36:02 +0900

    feat: NotificationLayout 구현

---
+++ src/app/(shared)/(standard)/mypage/notification/layout.tsx
@@ -0,0 +1,13 @@
+import * as S from './layout.styled';
+
+export default function NotificationLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <S.NotificationLayout>
+      <S.TextWrapper>
+        <S.Heading>푸시 및 카톡 알림</S.Heading>
+        <S.Description>알림을 켜 두면 코그룸과 함께 더욱 성장할 수 있어요.</S.Description>
+      </S.TextWrapper>
+      {children}
+    </S.NotificationLayout>
+  );
+}
