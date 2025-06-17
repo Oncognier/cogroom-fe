e5584008fb6202208fc0bcb1db2cb951a7efe372
오지훈 <gfsd2397@gmail.com>
2025-06-18 02:51:32 +0900

    feat: cogpoint 생성

---
+++ src/app/(shared)/(anomaly)/(home)/_components/Cogpoint/Cogpoint.tsx
@@ -0,0 +1,12 @@
+import Section from '../Section/Section';
+
+export default function Cogpoint() {
+  return (
+    <Section
+      title='코그 포인트'
+      subtitle='단순한 학습이 아닌 나를 알아가고 이해하는 여정을 제공합니다'
+    >
+      <div>asd</div>
+    </Section>
+  );
+}
