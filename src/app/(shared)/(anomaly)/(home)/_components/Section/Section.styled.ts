ce2f0e0ce158ce3f9cb1ba3097e022418ef7c589
오지훈 <gfsd2397@gmail.com>
2025-06-18 02:36:08 +0900

    feat: Section 추상화

---
+++ src/app/(shared)/(anomaly)/(home)/_components/Section/Section.styled.ts
@@ -0,0 +1,22 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing[8]};
+  padding-bottom: ${({ theme }) => theme.spacing[40]};
+`;
+
+export const Title = styled.h2`
+  ${({ theme }) => theme.typography.title2.bold}
+`;
+
+export const Subtitle = styled.p`
+  ${({ theme }) => theme.typography.body1.regular}
+`;
+
+export const Content = styled.div`
+`;
