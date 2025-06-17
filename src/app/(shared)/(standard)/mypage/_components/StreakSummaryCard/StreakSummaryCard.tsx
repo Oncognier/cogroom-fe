04df9b9f8bf313020895e60c5b83f3e0bb124921
Seio924 <seio9241@gmail.com>
2025-06-18 02:07:16 +0900

    feat: 사용자 대시보드 정보 조회 api 연결

--- src/app/(shared)/(standard)/mypage/_components/StreakSummaryCard/StreakSummaryCard.tsx
+++ src/app/(shared)/(standard)/mypage/_components/StreakSummaryCard/StreakSummaryCard.tsx
@@ -1,12 +1,15 @@
 import TextButton from '@/components/atoms/TextButton/TextButton';
 import * as S from './StreakSummaryCard.styled';
 
-export default function StreakSummaryCard() {
+interface StreakSummaryCardProps {
+  streakDays?: number;
+}
+export default function StreakSummaryCard({ streakDays }: StreakSummaryCardProps) {
   return (
     <S.StreakSummaryCard>
       <S.TextWrapper>
         <S.MainText>데일리 스트릭 </S.MainText>
-        <S.StreakDayText>10일째</S.StreakDayText>
+        <S.StreakDayText>{streakDays}일째</S.StreakDayText>
       </S.TextWrapper>
 
       <TextButton
--- src/app/(shared)/(standard)/mypage/page.tsx
+++ src/app/(shared)/(standard)/mypage/page.tsx
@@ -5,17 +5,22 @@ import * as S from './page.styled';
 import { REPORT_IMAGE } from '@/constants/image';
 import StreakSummaryCard from './_components/StreakSummaryCard/StreakSummaryCard';
 import ContentRecordToggle from './_components/ContentRecordToggle/ContentRecordToggle';
+import useGetUserDashboardQuery from '@/hooks/api/member/useGetUserDashboard';
 
 export default function Dashboard() {
+  const { data, isLoading } = useGetUserDashboardQuery();
+
+  if (isLoading) return <div> Loading...</div>;
+
   return (
     <S.DashboardContainer>
       <S.TextWrapper>
         <S.StreakWrapper>
           <S.StreakMessage>코그룸과</S.StreakMessage>
-          <S.Highlight>71일째</S.Highlight>
+          <S.Highlight>{data?.signupDays}일째</S.Highlight>
           <S.StreakMessage>함께 했어요</S.StreakMessage>
         </S.StreakWrapper>
-        <S.GreetingMessage>안녕, 홍길동 코그니어</S.GreetingMessage>
+        <S.GreetingMessage>안녕, {data?.nickname} 코그니어</S.GreetingMessage>
         <S.ChronotypeMessage>올빼미형은 주로 밤에 활기찬 성향을 가지고 있어요</S.ChronotypeMessage>
       </S.TextWrapper>
 
@@ -29,7 +34,7 @@ export default function Dashboard() {
       </S.Report>
 
       <S.SummarySectionWrapper>
-        <StreakSummaryCard />
+        <StreakSummaryCard streakDays={data?.streakDays} />
         <ContentRecordToggle />
       </S.SummarySectionWrapper>
     </S.DashboardContainer>
