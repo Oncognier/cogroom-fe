9d903e96a6b52a47bf01d58b3d68e10ad31c69e5
오지훈 <gfsd2397@gmail.com>
2025-06-18 02:51:51 +0900

    feat: home

---
+++ src/app/(shared)/(anomaly)/(home)/page.styled.ts
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const ContentsWrapper = styled.div`
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 4rem 0 2rem;
+`;
+
+export const Space = styled.div`
+  height: 12rem;
+`;
--- src/app/(shared)/(anomaly)/(home)/page.tsx
+++ src/app/(shared)/(anomaly)/(home)/page.tsx
@@ -1,3 +1,23 @@
+import Cogpoint from './_components/Cogpoint/Cogpoint';
+import Community from './_components/Community/Community';
+import Contents from './_components/Contents/Contents';
+import Daily from './_components/Daily/Daily';
+import Hero from './_components/Hero/Hero';
+import * as S from './page.styled';
+
 export default function Home() {
-  return <div>Root</div>;
+  return (
+    <>
+      <Hero />
+      <S.ContentsWrapper>
+        <Cogpoint />
+      </S.ContentsWrapper>
+      <S.Space />
+      <Daily />
+      <S.Space />
+      <Contents />
+      <S.Space />
+      <Community />
+    </>
+  );
 }
