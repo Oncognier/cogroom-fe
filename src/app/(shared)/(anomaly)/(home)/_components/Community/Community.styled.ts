bc9218f7ac8745b75b55058cc64bd163283ba332
오지훈 <gfsd2397@gmail.com>
2025-06-18 02:35:10 +0900

    feat: home - community

---
+++ src/app/(shared)/(anomaly)/(home)/_components/Community/Community.styled.ts
@@ -0,0 +1,56 @@
+'use client';
+
+import styled from '@emotion/styled';
+import Image from 'next/image';
+
+export const Wrapper = styled.div`
+  position: relative;
+  max-width: 1100px;
+  width: 100%;
+  height: 24rem;
+  margin: 0 auto;
+  overflow: hidden;
+`;
+
+export const CommunityImage = styled(Image)`
+  object-fit: cover;
+  z-index: -1;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing[24]};
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: ${({ theme }) => theme.spacing[8]};
+`;
+
+export const Title = styled.p`
+  ${({ theme }) => theme.typography.title2.bold}
+  color: ${({ theme }) => theme.semantic.primary.normal};
+`;
+
+export const SubTitle = styled.p`
+  ${({ theme }) => theme.typography.body1.semibold}
+  color: ${({ theme }) => theme.semantic.primary.normal};
+`;
+
+export const Content = styled.div`
+  ${({ theme }) => theme.typography.body2.regular}
+  color: ${({ theme }) => theme.semantic.label.neutral};
+`;
