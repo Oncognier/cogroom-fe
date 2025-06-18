7983a3d56b118dcc0109124fc5d20fc65322861c
오지훈 <gfsd2397@gmail.com>
2025-06-19 05:10:33 +0900

    feat: home - cogpoint

---
+++ src/app/(shared)/(anomaly)/(home)/_components/Cogpoint/Cogpoint.styled.ts
@@ -0,0 +1,73 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  max-width: 1100px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 2rem;
+`;
+
+export const CardList = styled.section`
+  display: flex;
+  justify-content: center;
+  gap: ${({ theme }) => theme.spacing[16]};
+`;
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+
+  border-radius: ${({ theme }) => theme.radius[12]};
+
+  box-shadow:
+    0px 0px 1px rgba(0, 0, 0, 0.08),
+    0px 1px 4px rgba(0, 0, 0, 0.08),
+    0px 2px 8px rgba(0, 0, 0, 0.12);
+  transition: transform 0.3s ease, z-index 0.3s ease;
+  position: relative;
+  z-index: 1;
+
+  &:hover {
+    transform: scale(1.07);
+    z-index: 10;
+  }
+`;
+
+export const Image = styled.img`
+  width: 30rem;
+  height: 17rem;
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing[8]};
+
+  padding: 4.8rem 3rem 3rem;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing[4]};
+`;
+
+export const Subtitle = styled.p`
+  ${({ theme }) => theme.typography.label1.semibold}
+  color: ${({ theme }) => theme.semantic.primary.normal};
+`;
+
+export const Title = styled.p`
+  ${({ theme }) => theme.typography.headline2.semibold}
+  color: ${({ theme }) => theme.semantic.static.black};
+`;
+
+export const Content = styled.p`
+  ${({ theme }) => theme.typography.label1.regular}
+  color: ${({ theme }) => theme.semantic.label.alternative};
+`;
--- src/app/(shared)/(anomaly)/(home)/_components/Cogpoint/Cogpoint.tsx
+++ src/app/(shared)/(anomaly)/(home)/_components/Cogpoint/Cogpoint.tsx
@@ -1,12 +1,76 @@
 import Section from '../Section/Section';
 
+import * as S from './Cogpoint.styled';
+
+const cards = [
+  {
+    src: '/cogpoint1.png',
+    subtitle: 'for self growth',
+    title: (
+      <>
+        나보다 나를 더 잘 아는 <br />
+        자기이해 플랫폼
+      </>
+    ),
+    content: (
+      <>
+        무의식적으로 해온 생각과 나도 몰랐던 나의<br />
+        마음들을 알아차려요
+      </>
+    ),
+  },
+  {
+    src: '/cogpoint2.png',
+    subtitle: 'scientific method',
+    title: (
+      <>
+        6가지 인지과학 분야로<br />
+        체계적인 성장을
+      </>
+    ),
+    content: '단순한 학습이 아닌, 과학적 학문 기반으로',
+  },
+  {
+    src: '/cogpoint3.png',
+    subtitle: 'for self growth',
+    title: (
+      <>
+        나보다 나를 더 잘 아는<br />
+        자기이해 플랫폼
+      </>
+    ),
+    content: (
+      <>
+        무의식적으로 해온 생각과 나도 몰랐던 나의<br />
+        마음들을 알아차려요
+      </>
+    ),
+  },
+];
+
 export default function Cogpoint() {
   return (
     <Section
       title='코그 포인트'
       subtitle='단순한 학습이 아닌 나를 알아가고 이해하는 여정을 제공합니다'
     >
-      <div>asd</div>
+      <S.Wrapper>
+        <S.CardList>
+          {cards.map((card, index) => (
+            <S.Card key={`cogpoint-${index}`}>
+              <S.Image
+                src={card.src}
+                alt=''
+              />
+              <S.TextWrapper>
+                <S.Subtitle>{card.subtitle}</S.Subtitle>
+                <S.Title>{card.title}</S.Title>
+                <S.Content>{card.content}</S.Content>
+              </S.TextWrapper>
+            </S.Card>
+          ))}
+        </S.CardList>
+      </S.Wrapper>
     </Section>
   );
 }
