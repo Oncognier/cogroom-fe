ef99946003696d9f3ff2800a921941de77420d7a
Seio924 <seio9241@gmail.com>
2025-06-18 16:37:42 +0900

    feat: 메타데이터 삽입

--- src/app/(shared)/(standard)/daily/layout.tsx
+++ src/app/(shared)/(standard)/daily/layout.tsx
@@ -1,6 +1,20 @@
 import Breadcrumb from '@/components/molecules/Breadcrumb/Breadcrumb';
+import { REPORT_IMAGE } from '@/constants/image';
 
 import * as S from './layout.styled';
+
+export async function generateMetadata() {
+  return {
+    title: '🔥 나의 데일리 스트릭',
+    description: '오늘도 코그룸과 함께 성장 중!',
+    openGraph: {
+      title: '🔥 나의 데일리 스트릭',
+      description: '오늘도 코그룸과 함께 성장 중!',
+      images: [REPORT_IMAGE],
+      url: 'https://cogroom.com/daily',
+    },
+  };
+}
 
 export default function DailyLayout({ children }: { children: React.ReactNode }) {
   return (
