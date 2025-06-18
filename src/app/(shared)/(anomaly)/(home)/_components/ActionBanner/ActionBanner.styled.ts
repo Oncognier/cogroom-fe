ef16ef812a71347cf71458bf98b3f3959ec42911
오지훈 <gfsd2397@gmail.com>
2025-06-19 05:12:10 +0900

    feat: home - actionbanner

---
+++ src/app/(shared)/(anomaly)/(home)/_components/ActionBanner/ActionBanner.styled.ts
@@ -0,0 +1,54 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: ${({ theme }) => theme.spacing[40]} 2rem 0;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 7rem;
+  width: 100%;
+  height: 8.5rem;
+
+  background-color: ${({ theme }) => theme.semantic.background.normal.alternative};
+  border-radius: ${({ theme }) => theme.radius[20]};
+`;
+
+export const TextWrapper = styled.div`
+  ${({ theme }) => theme.typography.body1.semibold}
+  color: ${({ theme }) => theme.semantic.primary.normal};
+`;
+
+export const ButtonWrapper = styled.button`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+  padding: 1.1rem 2.3rem;
+
+  border-radius: ${({ theme }) => theme.radius[12]};
+  border: 1px solid ${({ theme }) => theme.semantic.label.assistive};
+  background-color: ${({ theme }) => theme.semantic.static.white};
+  color: ${({ theme }) => theme.semantic.label.normal};
+
+  ${({ theme }) => theme.typography.body2.semibold};
+
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    border-color: ${({ theme }) => theme.semantic.label.assistive};
+    color: ${({ theme }) => theme.semantic.label.assistive};
+    cursor: default;
+    pointer-events: none;
+  }
+`;
---
+++ src/app/(shared)/(anomaly)/(home)/_components/ActionBanner/ActionBanner.tsx
@@ -0,0 +1,14 @@
+import * as S from './ActionBanner.styled';
+
+export default function ActionBanner() {
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <S.TextWrapper>지금 나만의 코그룸을 만들어보세요</S.TextWrapper>
+        <S.ButtonWrapper>
+          코그룸 시작하기
+        </S.ButtonWrapper>
+      </S.Wrapper>
+    </S.Container>
+  );
+}
