d1232be7c7ea948a726f84fd26437ae8e082f7aa
Seio924 <seio9241@gmail.com>
2025-06-20 06:29:00 +0900

    refactor: interceptors 리팩토링

--- src/api/axios/responseInterceptors.ts
+++ src/api/axios/responseInterceptors.ts
@@ -1,64 +1,41 @@
-import type { AxiosError, InternalAxiosRequestConfig } from 'axios';
-import axios from 'axios';
+import type { AxiosError } from 'axios';
 
+import { authApi } from '@/api/authApis';
 import { ERROR_CODE, HTTP_STATUS_CODE } from '@/constants/api';
 import { useAuthStore } from '@/stores/useAuthStore';
 
-import { authApi } from '../authApis';
 import { axiosInstance } from './axiosInstance';
 import { HTTPError } from './errors/HTTPError';
+import { ErrorResponseData } from './types';
 
-export interface ErrorResponseData {
-  statusCode?: number;
-  message?: string;
-  code?: string;
-}
-
-export const checkAndSetToken = (config: InternalAxiosRequestConfig) => {
-  if (!config.useAuth || !config.headers || config.headers.Authorization) {
-    delete axios.defaults.headers.common.Authorization;
-    return config;
-  }
-
-  const { accessToken, clearToken } = useAuthStore.getState();
-
-  if (!accessToken) {
-    clearToken();
-    if (typeof window !== 'undefined') {
-      window.location.href = '/';
-    }
-    return config;
-  }
-
-  config.headers.Authorization = `Bearer ${accessToken}`;
-  return config;
-};
+const isServer = typeof window === 'undefined';
 
 export const handleTokenError = async (error: AxiosError<ErrorResponseData>) => {
   const originalRequest = error.config;
 
-  if (!error.response || !originalRequest) throw new Error('에러가 발생했습니다.');
+  if (!error.response || !originalRequest) throw error;
 
   const { data, status } = error.response;
 
   if (status === HTTP_STATUS_CODE.UNAUTHORIZED && data.code === ERROR_CODE.TOKEN_EXPIRED_ERROR) {
     try {
       const { accessToken: newAccessToken } = await authApi.reissueToken();
-
-      if (!newAccessToken) throw new Error('토큰 재발급 실패');
+      if (!newAccessToken)
+        throw new HTTPError(
+          HTTP_STATUS_CODE.UNAUTHORIZED,
+          '토큰 재발급에 실패했습니다',
+          ERROR_CODE.TOKEN_REISSUE_FAILED,
+        );
 
       const { setToken } = useAuthStore.getState();
       setToken(newAccessToken);
 
       originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-
       return axiosInstance(originalRequest);
     } catch (reissueError) {
       const { clearToken } = useAuthStore.getState();
       clearToken();
-      if (typeof window !== 'undefined') {
-        window.location.href = '/';
-      }
+      if (!isServer) window.location.href = '/';
       throw reissueError;
     }
   }
@@ -67,14 +44,10 @@ export const handleTokenError = async (error: AxiosError<ErrorResponseData>) => 
     status === HTTP_STATUS_CODE.UNAUTHORIZED &&
     (data.code === ERROR_CODE.TOKEN_INVALID_ERROR || data.code === ERROR_CODE.ALREADY_BLACK_LIST)
   ) {
-    delete axios.defaults.headers.common.Authorization;
-
     const { clearToken } = useAuthStore.getState();
     clearToken();
 
-    if (typeof window !== 'undefined') {
-      window.location.href = '/';
-    }
+    if (!isServer) window.location.href = '/';
 
     throw new HTTPError(status, data.message, data.code);
   }
@@ -88,7 +61,7 @@ export const handleAPIError = (error: AxiosError<ErrorResponseData>) => {
   const { data, status } = error.response;
 
   if (status >= HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR) {
-    throw new HTTPError(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR, data.message);
+    throw new HTTPError(status, data.message);
   }
 
   throw new HTTPError(status, data.message, data.code);
