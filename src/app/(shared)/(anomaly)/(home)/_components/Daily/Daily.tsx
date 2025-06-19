7e980a599013b2afe83d810a63991fd44c3fe461
오지훈 <gfsd2397@gmail.com>
2025-06-20 02:40:26 +0900

    fix: Daily - mp4 -> image 전환

--- src/app/(shared)/(anomaly)/(home)/_components/Daily/Daily.tsx
+++ src/app/(shared)/(anomaly)/(home)/_components/Daily/Daily.tsx
@@ -8,33 +8,11 @@ export default function Daily() {
       subtitle='단순한 학습이 아닌 나를 알아가고 이해하는 여정을 제공합니다'
     >
       <S.Wrapper>
-        <S.Video
-          src='/waterdrop.mp4'
-          autoPlay
-          loop
-          muted
+        <S.DailyImage
+          src='/waterdrop.png'
+          alt='daily'
+          fill
         />
-        <S.DimmedOverlay />
-        <S.ContentsWrapper>
-          <S.TitleWrapper>
-            <S.Title>하루 30초, 한 걸음 나아가기</S.Title>
-            <S.SubTitle>쉽고 부담없는 데일리 질문으로 일상에서 사유를 습관화합니다</S.SubTitle>
-          </S.TitleWrapper>
-          <S.QuestionWrapper>
-            <S.LeftQuestion>
-              <S.Question>지금 드는 감정은...</S.Question>
-            </S.LeftQuestion>
-            <S.RightQuestion>
-              <S.Question>너에게 ‘삶’이란 어떤 의미야?</S.Question>
-            </S.RightQuestion>
-            <S.CenterQuestion>
-              <S.Question>
-                감정이 없는 하루, 감정을 주체할 수 없는 하루
-                <br />둘 중 하나를 골라 살아야 한다면?
-              </S.Question>
-            </S.CenterQuestion>
-          </S.QuestionWrapper>
-        </S.ContentsWrapper>
       </S.Wrapper>
     </Section>
   );
