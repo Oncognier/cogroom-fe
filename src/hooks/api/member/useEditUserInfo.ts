99db099783d895ba38f3736d38b4e279ba283d93
Seio924 <seio9241@gmail.com>
2025-06-21 02:22:55 +0900

    refactor: setValidationError 유틸 함수로 분리

--- src/hooks/api/member/useEditUserInfo.ts
+++ src/hooks/api/member/useEditUserInfo.ts
@@ -4,8 +4,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { memberApi } from '@/api/memberApis';
 import { MEMBER_QUERY_KEYS } from '@/constants/queryKeys';
 import { SettingFormFields } from '@/app/(shared)/(standard)/mypage/setting/page';
-import { ErrorResponseData } from '@/api/axios/types';
-import { VALIDATION_FIELD_MAP, VALIDATION_MESSAGE, ValidationErrorKey } from '@/constants/validationMessages';
+import { setValidationError } from '@/utils/validators/setValidationError';
 
 export const useEditUserInfoMutation = (setError?: UseFormSetError<SettingFormFields>) => {
   const queryClient = useQueryClient();
@@ -15,15 +14,8 @@ export const useEditUserInfoMutation = (setError?: UseFormSetError<SettingFormFi
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [...MEMBER_QUERY_KEYS.MEMBER_SUMMARY] });
     },
-    onError: (error: ErrorResponseData) => {
-      const code = error.code as ValidationErrorKey;
-      const field = VALIDATION_FIELD_MAP[code];
-
-      if (field && setError) {
-        setError(field as keyof SettingFormFields, {
-          message: VALIDATION_MESSAGE[code],
-        });
-      } else alert('정보 수정에 실패했습니다. 잠시 후 다시 시도해주세요.');
+    onError: (error) => {
+      setValidationError<SettingFormFields>(error, setError);
     },
   });
 
