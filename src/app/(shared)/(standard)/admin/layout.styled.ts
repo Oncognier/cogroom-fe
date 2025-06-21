2f4b4011dbff5ca893b8d3203b307c50c4723889
Seio924 <seio9241@gmail.com>
2025-06-21 19:00:55 +0900

    feat: 어드민 페이지 라우팅 구조 구현

---
+++ src/app/(shared)/(standard)/admin/layout.styled.ts
@@ -0,0 +1,15 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const AdminLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+`;
+
+export const AdminHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
