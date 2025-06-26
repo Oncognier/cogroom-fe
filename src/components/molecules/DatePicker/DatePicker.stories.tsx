1d3a8dc145976e0d58fc7b2693bfc85669cddc59
오지훈 <gfsd2397@gmail.com>
2025-06-26 17:15:05 +0900

    feat: datepicker storybook

---
+++ src/components/molecules/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import dayjs from 'dayjs';
+import { useArgs } from '@storybook/preview-api';
+import DatePicker from './DatePicker';
+
+const meta: Meta<typeof DatePicker> = {
+  title: 'Components/Molecules/DatePicker',
+  component: DatePicker,
+  tags: ['autodocs'],
+  argTypes: {
+    onSelect: { action: 'selected' },
+    onClose: { action: 'closed' },
+    isOpen: { control: 'boolean' },
+    selectedDate: { control: 'date' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof DatePicker>;
+
+export const Default: Story = {
+  args: {
+    isOpen: true,
+    selectedDate: dayjs(),
+  },
+  render: (args) => {
+    const [{ selectedDate }, updateArgs] = useArgs();
+
+    return (
+      <DatePicker
+        {...args}
+        selectedDate={selectedDate ? dayjs(selectedDate) : null}
+        onSelect={(date) => {
+          updateArgs({ selectedDate: date });
+        }}
+        onClose={() => updateArgs({ isOpen: false })}
+      />
+    );
+  },
+};
