03083c5fef120f46e6631097c099a03e827ba219
Seio924 <seio9241@gmail.com>
2025-06-26 18:52:25 +0900

    feat: SelectTagList overflow 설정

--- src/components/molecules/Select/SelectTagList/SelectTagList.styled.ts
+++ src/components/molecules/Select/SelectTagList/SelectTagList.styled.ts
@@ -6,4 +6,14 @@ export const SelectTagList = styled.div`
   display: flex;
   align-items: center;
   gap: 0.4rem;
+
+  overflow: hidden;
+  overflow-x: auto;
+  white-space: nowrap;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 `;
