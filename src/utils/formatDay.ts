8552328b4d1fcadf6057b109dc09cd1495fb6ee4
오지훈 <gfsd2397@gmail.com>
2025-06-26 16:53:57 +0900

    feat: date 추가 유틸

--- src/utils/formatDay.ts
+++ src/utils/formatDay.ts
@@ -6,6 +6,10 @@ export function formatDayAsDashYYYYMMDD(date: Dayjs): string {
   return date.format('YYYY-MM-DD');
 }
 
+export function formatDayAsDashYYMMDD(date: Dayjs): string {
+  return date.format('YY-MM-DD');
+}
+
 export function formatDayAsDotYYMMDD(date: Dayjs): string {
   return date.format('YY.MM.DD');
 }
@@ -22,7 +26,24 @@ export function formatDayAsSlashMMDD(date: Dayjs): string {
   return date.format('MM/DD');
 }
 
+export function formatDayAsYYYYMM(date: Dayjs): string {
+  return date.format('YYYY년 MM월');
+}
+
 export function formatDayAsWeekday(date: Dayjs): string {
-  const dayIndex = date.day(); // dayjs에서 0: 일요일 ~ 6: 토요일
+  const dayIndex = date.day();
   return WEEK_DAYS[dayIndex === 0 ? 6 : dayIndex - 1];
 }
+
+export function formatDateRangeLabel(selectedStartDate?: Dayjs | null, selectedEndDate?: Dayjs | null): string {
+  if (selectedStartDate && selectedEndDate) {
+    return `${formatDayAsSlashYYMMDD(selectedStartDate)} ~ ${formatDayAsSlashYYMMDD(selectedEndDate)}`;
+  } else if (selectedStartDate) {
+    return `${formatDayAsSlashYYMMDD(selectedStartDate)} ~`;
+  } else if (selectedEndDate) {
+    return `~ ${formatDayAsSlashYYMMDD(selectedEndDate)}`;
+  } else {
+    return '';
+  }
+}
+
