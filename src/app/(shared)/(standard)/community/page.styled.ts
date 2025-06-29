2998d09d3b44194eb6bee3d57e64bf2cf8536184
오지훈 <gfsd2397@gmail.com>
2025-06-30 07:57:45 +0900

    feat: 커뮤니티 배너

---
+++ src/app/(shared)/(standard)/community/page.styled.ts
@@ -0,0 +1,43 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+`;
+
+export const BannerWrapper = styled.div`
+  position: relative;
+  aspect-ratio: 1060 / 566;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 1.2rem;
+  padding: 0.4rem;
+
+  img {
+    object-fit: contain;
+    z-index: -1;
+  }
+`;
+
+export const BannerTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.8rem;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const BannerTitle = styled.p`
+  ${({ theme }) => theme.typography.title1.bold}
+  color: ${({ theme }) => theme.semantic.label.normal};
+`;
--- src/app/(shared)/(standard)/community/page.tsx
+++ src/app/(shared)/(standard)/community/page.tsx
@@ -1,3 +1,31 @@
+import Image from 'next/image';
+
+import Breadcrumb from '@/components/molecules/Breadcrumb/Breadcrumb';
+import { DEFAULT_COMMUNITY_MAIN_BANNER } from '@/constants/image';
+
+import * as S from './page.styled';
+
 export default function Community() {
-  return <div>community</div>;
+  return (
+    <S.ContentContainer>
+      <Breadcrumb
+        items={[
+          { name: '홈', href: '/' },
+          { name: '커뮤니티', href: '/community' },
+        ]}
+      />
+      <S.BannerWrapper>
+        <Image
+          src={DEFAULT_COMMUNITY_MAIN_BANNER}
+          alt='community banner'
+          fill
+        />
+        {/* <S.BannerTitleWrapper>
+          <S.BannerTitle>서로 안아주기</S.BannerTitle>
+          <S.BannerTitle>더욱 알아가기</S.BannerTitle>
+          <S.BannerTitle>진짜 대화하기</S.BannerTitle>
+        </S.BannerTitleWrapper> */}
+      </S.BannerWrapper>
+    </S.ContentContainer>
+  );
 }
