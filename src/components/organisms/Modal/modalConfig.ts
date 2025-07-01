3d55a464a2f6cfb811debd1d0fff9d2dadecb3dc
오지훈 <gfsd2397@gmail.com>
2025-07-02 06:47:28 +0900

    fix: modalconfig 변경

--- src/components/organisms/Modal/modalConfig.ts
+++ src/components/organisms/Modal/modalConfig.ts
@@ -4,7 +4,6 @@ import Alert, { AlertProps } from './Alert/Alert';
 import ChangeRole, { ChangeRoleProps } from './ChangeRole/ChangeRole';
 import DailyShare, { DailyShareProps } from './Daily/DailyShare/DailyShare';
 import DailyAnswerEdit, { DailyAnswerEditProps } from './Daily/Edit/DailyAnswerEdit';
-import DailyFirstAnswer from './Daily/FirstAnswer/DailyFirstAnswer';
 import DailyAnswerPost, { DailyAnswerPostProps } from './Daily/Post/DailyAnswerPost';
 import Error, { ErrorProps } from './Error/Error';
 import Login from './Login/Login';
@@ -16,14 +15,13 @@ export type AppModalProps = {
   signup: SignupProps;
   logout: undefined;
   dailyAnswerPost: DailyAnswerPostProps;
-  dailyAnswerEdit: DailyAnswerEditProps;
-  dailyFirstAnswer: undefined;
   dailyShare: DailyShareProps;
 };
 
 export type AlertModalProps = {
   error: ErrorProps;
   alert: AlertProps;
+  dailyAnswerEdit: DailyAnswerEditProps;
   changeRole: ChangeRoleProps;
 };
 
@@ -32,13 +30,12 @@ export const AppModalRegistry = {
   signup: { Component: Signup, disableOutsideClick: true },
   logout: { Component: Logout, disableOutsideClick: false },
   dailyAnswerPost: { Component: DailyAnswerPost, disableOutsideClick: false },
-  dailyAnswerEdit: { Component: DailyAnswerEdit, disableOutsideClick: false },
-  dailyFirstAnswer: { Component: DailyFirstAnswer, disableOutsideClick: false },
   dailyShare: { Component: DailyShare, disableOutsideClick: false },
 } satisfies ModalMap<AppModalProps>;
 
 export const AlertModalRegistry = {
   error: { Component: Error, disableOutsideClick: false },
   alert: { Component: Alert, disableOutsideClick: false },
+  dailyAnswerEdit: { Component: DailyAnswerEdit, disableOutsideClick: false },
   changeRole: { Component: ChangeRole, disableOutsideClick: false },
 } satisfies ModalMap<AlertModalProps>;
