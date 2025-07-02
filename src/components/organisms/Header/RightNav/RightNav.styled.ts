1a3aaedb3090b158f87b69e1523bd5672c169e16
오지훈 <gfsd2397@gmail.com>
2025-07-02 17:56:56 +0900

    feat: header 권한 ui 반영

--- src/components/organisms/Header/RightNav/RightNav.styled.ts
+++ src/components/organisms/Header/RightNav/RightNav.styled.ts
@@ -13,3 +13,30 @@ export const NavLogin = styled.div`
   gap: 0.7rem;
   height: 2.6rem;
 `;
+
+export const UserWrapper = styled.div<{ memberRole?: string }>`
+  display: flex;
+  align-items: center;
+  gap: 0.7rem;
+  height: 4.2rem;
+
+  padding: 0.4rem 1.2rem 0.4rem 0.8rem;
+  border-radius: 1000px;
+
+  ${({ theme }) => theme.typography.label2.regular};
+  color: ${({ theme }) => theme.semantic.primary.normal};
+  border: ${({ theme, memberRole }) =>
+    memberRole === 'CONTENT_PROVIDER' && `1px solid ${theme.semantic.primary.normal}`};
+  background-color: ${({ theme, memberRole }) => memberRole === 'ADMIN' && theme.semantic.background.elevated.normal};
+
+  cursor: pointer;
+`;
+
+export const UserIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 2.6rem;
+  height: 2.6rem;
+`;
