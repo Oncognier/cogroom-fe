4712785f4374535e7a5800aa25385e30aac0f292
Seio924 <seio9241@gmail.com>
2025-07-03 14:26:50 +0900

    feat: 개인정보 설정 화면에 '상세 개인정보 설정' 버튼 추가

--- src/app/(shared)/(standard)/mypage/setting/page.tsx
+++ src/app/(shared)/(standard)/mypage/setting/page.tsx
@@ -3,13 +3,15 @@
 import { useEffect, useState } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 
+import SettingIcon from '@/assets/icons/setting.svg';
 import OutlinedButton from '@/components/atoms/OutlinedButton/OutlinedButton';
+import TextButton from '@/components/atoms/TextButton/TextButton';
 import Input from '@/components/molecules/Input/Input';
 import Textarea from '@/components/molecules/Textarea/Textarea';
 import Loading from '@/components/organisms/Loading/Loading';
 import { useEditUserInfoMutation } from '@/hooks/api/member/useEditUserInfo';
 import useGetUserInfo from '@/hooks/api/member/useGetUserInfo';
-import { useAlertModalStore } from '@/stores/useModalStore';
+import { useAlertModalStore, useAppModalStore } from '@/stores/useModalStore';
 import { SettingFormFields } from '@/types/form';
 import { formatPhoneNumber } from '@/utils/formatAutoComplete';
 import { validatePhoneNumber } from '@/utils/validators/userValidators';
@@ -35,7 +37,8 @@ export default function Setting() {
   const [emailState, setEmailState] = useState<EmailState>('idle');
   const [isNicknameChecked, setIsNicknameChecked] = useState(true);
 
-  const { open } = useAlertModalStore();
+  const { open: openApp } = useAppModalStore();
+  const { open: openAlert } = useAlertModalStore();
   const { data, isLoading } = useGetUserInfo();
 
   const methods = useForm<SettingFormFields>({
@@ -63,7 +66,7 @@ export default function Setting() {
 
   const onSubmit = (formData: SettingFormFields) => {
     if (dirtyFields.nickname && !isNicknameChecked) {
-      open('alert', { message: '닉네임 중복 확인을 완료해주세요.' });
+      openAlert('alert', { message: '닉네임 중복 확인을 완료해주세요.' });
       return;
     }
 
@@ -73,51 +76,62 @@ export default function Setting() {
   if (isLoading) return <Loading />;
 
   return (
-    <FormProvider {...methods}>
-      <S.SettingForm onSubmit={handleSubmit(onSubmit)}>
-        <SettingProfile initialImageUrl={data?.imageUrl} />
+    <S.SettingContainer>
+      <FormProvider {...methods}>
+        <S.SettingForm onSubmit={handleSubmit(onSubmit)}>
+          <SettingProfile initialImageUrl={data?.imageUrl} />
 
-        <NicknameForm onCheck={setIsNicknameChecked} />
+          <NicknameForm onCheck={setIsNicknameChecked} />
 
-        <EmailForm
-          emailState={emailState}
-          setEmailState={setEmailState}
-        />
-
-        <Input
-          inputSize='md'
-          label='전화번호'
-          {...register('phoneNumber', {
-            validate: validatePhoneNumber,
-          })}
-          onBlur={(e) => {
-            const formatted = formatPhoneNumber(e.target.value);
-            setValue('phoneNumber', formatted, { shouldValidate: true });
-          }}
-          error={errors.phoneNumber?.message}
-          width='34.5rem'
-        />
-
-        <Textarea
-          textareaSize='md'
-          label='자기소개'
-          {...register('description')}
-          error={errors.description?.message}
-          width='34.5rem'
-          minHeight='12.8rem'
-        />
-
-        <S.ButtonWrapper>
-          <OutlinedButton
-            size='md'
-            color='primary'
-            label='저장하기'
-            interactionVariant='normal'
-            type='submit'
-            isDisabled={!isValid || !isEmailStateValid(emailState)}
+          <EmailForm
+            emailState={emailState}
+            setEmailState={setEmailState}
           />
-        </S.ButtonWrapper>
-      </S.SettingForm>
-    </FormProvider>
+
+          <Input
+            inputSize='md'
+            label='전화번호'
+            {...register('phoneNumber', {
+              validate: validatePhoneNumber,
+            })}
+            onBlur={(e) => {
+              const formatted = formatPhoneNumber(e.target.value);
+              setValue('phoneNumber', formatted, { shouldValidate: true });
+            }}
+            error={errors.phoneNumber?.message}
+            width='34.5rem'
+          />
+
+          <Textarea
+            textareaSize='md'
+            label='자기소개'
+            {...register('description')}
+            error={errors.description?.message}
+            width='34.5rem'
+            minHeight='12.8rem'
+          />
+
+          <S.ButtonWrapper>
+            <OutlinedButton
+              size='md'
+              color='primary'
+              label='저장하기'
+              interactionVariant='normal'
+              type='submit'
+              isDisabled={!isValid || !isEmailStateValid(emailState)}
+            />
+          </S.ButtonWrapper>
+        </S.SettingForm>
+      </FormProvider>
+
+      <TextButton
+        size='lg'
+        label='상세 개인정보 설정'
+        color='assistive'
+        iconRight={<SettingIcon />}
+        interactionVariant='normal'
+        onClick={() => openApp('withdraw')}
+      />
+    </S.SettingContainer>
   );
 }
