f485ffb0de3e6cd24f058a199c3ded4265d6189b
Seio924 <seio9241@gmail.com>
2025-07-03 14:25:55 +0900

    feat: 탈퇴 이유 입력 단계 구현

---
+++ src/components/organisms/Modal/Withdraw/Steps/InputReason/InputReason.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useFormContext } from 'react-hook-form';
+
+import OutlinedButton from '@/components/atoms/OutlinedButton/OutlinedButton';
+import Textarea from '@/components/molecules/Textarea/Textarea';
+
+import * as S from './InputReason.styled';
+
+export interface InputReasonProps {
+  onConfirm: () => void;
+}
+
+export default function InputReason({ onConfirm }: InputReasonProps) {
+  const { register } = useFormContext<{ reason: string }>();
+
+  return (
+    <S.InputReason>
+      <S.Title>이유를 알려줄 수 있어요?</S.Title>
+
+      <Textarea
+        textareaSize='lg'
+        minHeight='15.5rem'
+        {...register('reason', { required: true })}
+      />
+
+      <OutlinedButton
+        type='submit'
+        size='lg'
+        color='primary'
+        label='제출하기'
+        fillContainer
+        interactionVariant='normal'
+        onClick={onConfirm}
+      />
+    </S.InputReason>
+  );
+}
