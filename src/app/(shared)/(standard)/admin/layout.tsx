bef0be10c9e21dc51ef262d1c9d681ed33fc1838
오지훈 <gfsd2397@gmail.com>
2025-07-04 20:59:10 +0900

    feat: adminguard 적용

--- src/app/(shared)/(standard)/admin/layout.tsx
+++ src/app/(shared)/(standard)/admin/layout.tsx
@@ -1,9 +1,29 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
 import Breadcrumb from '@/components/molecules/Breadcrumb/Breadcrumb';
 import TabBarList from '@/components/molecules/TabBarList/TabBarList';
-
+import AdminGuard from '@/components/organisms/Guard/AdminGuard/AdminGuard';
+import useGetUserSummary from '@/hooks/api/member/useGetUserSummary';
+import Loading from '@/components/organisms/Loading/Loading';
 import * as S from './layout.styled';
+import { getAdminTabState } from './_utils/getAdminTabState';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  const { data, isLoading } = useGetUserSummary();
+  const pathname = usePathname();
+
+  const role = data?.memberRole;
+
+  if (isLoading) return <Loading />;
+
+  if (role === 'USER' || role === undefined) return <AdminGuard />;
+
+  if (role === 'CONTENT_PROVIDER' && pathname !== '/admin' && !pathname.includes('/admin/contents')) {
+    return <AdminGuard />;
+  }
+
   return (
     <S.AdminLayout>
       <S.AdminHeader>
@@ -15,10 +35,26 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
         />
         <TabBarList
           items={[
-            { label: '공지사항', href: '/admin/notices' },
-            { label: '회원관리', href: '/admin/users' },
-            { label: '결제관리', href: '/admin/payments' },
-            { label: '콘텐츠 관리', href: '/admin/contents' },
+            {
+              label: '공지사항',
+              href: '/admin/notices',
+              state: getAdminTabState(pathname, '/admin/notices', role),
+            },
+            {
+              label: '회원관리',
+              href: '/admin/users',
+              state: getAdminTabState(pathname, '/admin/users', role),
+            },
+            {
+              label: '결제관리',
+              href: '/admin/payments',
+              state: getAdminTabState(pathname, '/admin/payments', role),
+            },
+            {
+              label: '콘텐츠 관리',
+              href: '/admin/contents',
+              state: getAdminTabState(pathname, '/admin/contents', role),
+            },
           ]}
           size='md'
           interactionVariant='normal'
