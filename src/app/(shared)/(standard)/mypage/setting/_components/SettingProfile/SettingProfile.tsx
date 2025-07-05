9fb2519adabe5da69349244a0b178bb403a259bf
Seio924 <seio9241@gmail.com>
2025-07-05 18:34:49 +0900

    feat: 기본 이미지 설정 추가

--- src/app/(shared)/(standard)/mypage/setting/_components/SettingProfile/SettingProfile.tsx
+++ src/app/(shared)/(standard)/mypage/setting/_components/SettingProfile/SettingProfile.tsx
@@ -6,21 +6,21 @@ import { useFormContext } from 'react-hook-form';
 import SettingIcon from '@/assets/icons/setting.svg';
 import AvatarPerson from '@/components/atoms/AvatarPerson/AvatarPerson';
 import IconButton from '@/components/atoms/IconButton/IconButton';
+import { useDropdown } from '@/hooks/useDropdown';
 import { useUploadFileToS3Mutation } from '@/hooks/api/file/useUploadFileToS3';
 import { SettingFormFields } from '@/types/form';
 
 import * as S from './SettingProfile.styled';
+import MenuDropdown from './MenuDropdown/MenuDropdown';
 
 interface SettingProfileProps {
   initialImageUrl?: string;
 }
 
 export default function SettingProfile({ initialImageUrl }: SettingProfileProps) {
-  const inputRef = useRef<HTMLInputElement>(null);
   const { setValue, watch } = useFormContext<SettingFormFields>();
 
   const imageUrl = watch('imageUrl');
-
   const [preview, setPreview] = useState<string | undefined>(initialImageUrl);
 
   useEffect(() => {
@@ -34,14 +34,24 @@ export default function SettingProfile({ initialImageUrl }: SettingProfileProps)
     },
   });
 
-  const handleClick = () => inputRef.current?.click();
+  const { isOpen, toggle, close, handleBlur, dropdownRef } = useDropdown();
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const triggerSelect = () => fileInputRef.current?.click();
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
-
     setPreview(URL.createObjectURL(file));
     uploadToS3({ files: [file] });
+    close();
+  };
+
+  const handleResetToDefault = () => {
+    setValue('imageUrl', undefined, { shouldValidate: true });
+    setPreview(undefined);
+    close();
   };
 
   return (
@@ -52,25 +62,39 @@ export default function SettingProfile({ initialImageUrl }: SettingProfileProps)
         src={preview}
       />
 
-      <S.SetImage>
+      <input
+        ref={fileInputRef}
+        type='file'
+        accept='image/*'
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+        disabled={isPending}
+      />
+
+      <S.SetImage
+        ref={dropdownRef}
+        tabIndex={-1}
+        onBlur={handleBlur}
+      >
         <IconButton
           size='3.6rem'
           variant='outlined'
           interactionVariant='normal'
-          onClick={handleClick}
+          onClick={toggle}
           isDisabled={isPending}
         >
           <SettingIcon />
         </IconButton>
 
-        <input
-          ref={inputRef}
-          type='file'
-          accept='image/*'
-          style={{ display: 'none' }}
-          onChange={handleChange}
-          disabled={isPending}
-        />
+        {isOpen && (
+          <S.DropdownWrapper>
+            <MenuDropdown
+              triggerSelect={triggerSelect}
+              onResetToDefault={handleResetToDefault}
+              isUploading={isPending}
+            />
+          </S.DropdownWrapper>
+        )}
       </S.SetImage>
     </S.SettingProfile>
   );
