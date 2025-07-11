50ab3cef5d3ac0f6e35621a55dd82fb197d7e64a
오지훈 <gfsd2397@gmail.com>
2025-07-11 11:33:22 +0900

    feat: dailyfirshanswer 모달 생성

--- src/components/organisms/Modal/Daily/FirstAnswer/DailyFirstAnswer.tsx
+++ src/components/organisms/Modal/Daily/FirstAnswer/DailyFirstAnswer.tsx
@@ -1,25 +1,34 @@
 'use client';
 
 import OutlinedButton from '@/components/atoms/OutlinedButton/OutlinedButton';
+import { useSubmitDailyAnswerMutation } from '@/hooks/api/daily/useSubmitDailyAnswer';
 import { useAppModalStore } from '@/stores/useModalStore';
 
 import * as S from './DailyFirstAnswer.styled';
 
-export default function DailyFirstAnswer() {
+export interface DailyFirstAnswerProps {
+  assignedQuestionId: number;
+  answer: string;
+}
+
+export default function DailyFirstAnswer({ assignedQuestionId, answer }: DailyFirstAnswerProps) {
   const { close } = useAppModalStore();
+  const { submitDailyAnswer } = useSubmitDailyAnswerMutation();
 
   return (
     <S.Container>
-      <S.TextWrapper>
-        <S.Title>작성한 답변은 오늘 자정까지만 바꿀 수 있어요</S.Title>
-      </S.TextWrapper>
+      <S.Title>작성한 답변은 오늘 자정까지만 바꿀 수 있어요</S.Title>
 
       <OutlinedButton
-        label='확인했어요'
+        label='확인'
         size='sm'
         color='assistive'
         interactionVariant='normal'
-        onClick={close}
+        fillContainer
+        onClick={async () => {
+          close();
+          await submitDailyAnswer({ assignedQuestionId, answer });
+        }}
         type='submit'
       />
     </S.Container>
