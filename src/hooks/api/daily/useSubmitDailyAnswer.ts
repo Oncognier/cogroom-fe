77a91700d82928b732e509e980fb108575ca2b85
Seio924 <134473627+Seio924@users.noreply.github.com>
2025-07-17 20:02:52 +0900

    [FEAT] 데일리 유효성 에러모달 서브문구 제거 #261 (#262)

--- src/hooks/api/daily/useSubmitDailyAnswer.ts
+++ src/hooks/api/daily/useSubmitDailyAnswer.ts
@@ -23,13 +23,13 @@ export const useSubmitDailyAnswerMutation = () => {
     onError: (error: HTTPError) => {
       switch (error.code) {
         case 'ANSWER_NOTBLANK_ERROR':
-          openAlert('error', { message: '한 글자라도 적어주세요' });
+          openAlert('alert', { message: '한 글자라도 적어주세요' });
           break;
         case 'ANSWER_SIZE_ERROR':
-          openAlert('error', { message: '내용을 조금만 줄여볼까요?' });
+          openAlert('alert', { message: '내용을 조금만 줄여볼까요?' });
           break;
         case 'ANSWER_TIME_EXPIRED':
-          openAlert('error', { message: '앗.. 자정이 지나 수정할 수 없어요' });
+          openAlert('alert', { message: '앗.. 자정이 지나 수정할 수 없어요' });
           break;
         default:
           openAlert('error', { message: error.message });
