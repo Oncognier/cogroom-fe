930a82d402dd569e20a73870dcd6553717d74d68
Seio924 <seio9241@gmail.com>
2025-07-17 14:28:21 +0900

    feat: 페이지 문구 변경

--- src/app/(shared)/(standard)/mypage/notification/layout.tsx
+++ src/app/(shared)/(standard)/mypage/notification/layout.tsx
@@ -5,7 +5,9 @@ export default function NotificationLayout({ children }: { children: React.React
     <S.NotificationLayout>
       <S.TextWrapper>
         <S.Heading>푸시 및 카톡 알림</S.Heading>
-        <S.Description>알림을 켜 두면 코그룸과 함께 더욱 성장할 수 있어요.</S.Description>
+        <S.Description>
+          알림을 켜 두면 코그룸과 함께 더욱 성장할 수 있어요. (*알림 기능은 아직 지원하지 않아요!)
+        </S.Description>
       </S.TextWrapper>
       {children}
     </S.NotificationLayout>
