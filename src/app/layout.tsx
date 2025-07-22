ae21f879b23c5d4b5fc1bf28280f7eb4d775cf16
Seio924 <134473627+Seio924@users.noreply.github.com>
2025-07-22 22:15:26 +0900

    [FEAT] GTM 추가 #278 (#279)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import { pretendard } from '@/styles/font';
 import KakaoInitializer from './KakaoInitializer';
 import ModalProvider from './ModalProvider';
 
-// msw 서버 적용
 mockingServer();
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -18,7 +17,35 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang='ko'
       className={pretendard.variable}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id='gtm-head'
+          strategy='beforeInteractive'
+        >
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+  `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            title='Google Tag Manager'
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Script
           src='https://t1.kakaocdn.net/kakao_js_sdk/2.7.5/kakao.min.js'
           integrity='sha384-dok87au0gKqJdxs7msEdBPNnKSRT+/mhTVzq+qOhcL464zXwvcrpjeWvyj1kCdq6'
