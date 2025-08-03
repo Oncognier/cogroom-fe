0f12c5af755372618e4354052ee94d9e8dd3df1f
오지훈 <gfsd2397@gmail.com>
2025-08-04 04:46:22 +0900

    feat: 2자리수 pad 함수

--- src/utils/formatText.ts
+++ src/utils/formatText.ts
@@ -1,3 +1,7 @@
+export const formatToTwoDigits = (num: number): string => {
+  return num.toString().padStart(2, '0');
+};
+
 export const formatToSixDigits = (num: number): string => {
   return num.toString().padStart(6, '0');
 };
