7210a43135404e517e242bfca99973e953b340d2
오지훈 <gfsd2397@gmail.com>
2025-08-04 04:57:26 +0900

    refactor: daily - Calendar date 관련 변경

--- src/app/(shared)/(standard)/daily/_components/Calendar/Calendar.tsx
+++ src/app/(shared)/(standard)/daily/_components/Calendar/Calendar.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import dayjs from 'dayjs';
 import { useRouter } from 'next/navigation';
 import { useEffect, useRef, useState, useMemo } from 'react';
 
@@ -11,7 +10,7 @@ import SolidButton from '@/components/atoms/SolidButton/SolidButton';
 import { WEEK_DAYS } from '@/constants/common';
 import { useAuthStore } from '@/stores/useAuthStore';
 import { useAppModalStore } from '@/stores/useModalStore';
-import { getCalendarMonthDates, getCalendarWeekDates } from '@/utils/getCalendar';
+import { getCalendarMonthDateStrings, getCalendarWeekDateStrings } from '@/utils/date/getCalendar';
 
 import * as S from './Calendar.styled';
 import DateCell from './DateCell/DataCell';
@@ -27,21 +26,14 @@ export default function Calendar({ streakDateList, hasAnswered }: CalendarProps)
   const { open } = useAppModalStore();
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
 
-  const today = dayjs();
+  const today = new Date();
   const [isMonthly, setIsMonthly] = useState(false);
-  const monthDates = useMemo(() => getCalendarMonthDates(today), [today]);
-  const weekDates = useMemo(() => getCalendarWeekDates(today), [today]);
+  const monthDates = useMemo(() => getCalendarMonthDateStrings(), []);
+  const weekDates = useMemo(() => getCalendarWeekDateStrings(), []);
   const dates = isMonthly ? monthDates : weekDates;
 
   const [isFirstAnswer, setIsFirstAnswer] = useState(false);
   const prevHasAnsweredRef = useRef<boolean>(hasAnswered);
-
-  useEffect(() => {
-    if (!prevHasAnsweredRef.current && hasAnswered) {
-      setIsFirstAnswer(true);
-    }
-    prevHasAnsweredRef.current = hasAnswered;
-  }, [hasAnswered]);
 
   const handleGoToReport = () => {
     if (!isLoggedIn) {
@@ -51,13 +43,20 @@ export default function Calendar({ streakDateList, hasAnswered }: CalendarProps)
     router.push('/mypage/activity');
   };
 
+  useEffect(() => {
+    if (!prevHasAnsweredRef.current && hasAnswered) {
+      setIsFirstAnswer(true);
+    }
+    prevHasAnsweredRef.current = hasAnswered;
+  }, [hasAnswered]);
+
   return (
     <S.CalendarCard>
       <S.CalendarContentWrapper>
         <S.Title>내 물방울 기록</S.Title>
         <S.CalendarWrapper>
           <S.MonthSelector>
-            {today.year()}년 {today.month() + 1}월
+            {today.getFullYear()}년 {today.getMonth() + 1}월
             <S.BreadcrumbChevron onClick={() => setIsMonthly((prev) => !prev)}>
               {isMonthly ? <ChevronUp /> : <ChevronDown />}
             </S.BreadcrumbChevron>
@@ -68,8 +67,8 @@ export default function Calendar({ streakDateList, hasAnswered }: CalendarProps)
             ))}
             {dates.map((date) => (
               <DateCell
-                key={date.toISOString()}
-                date={date}
+                key={date}
+                dateData={date}
                 streakDateList={streakDateList}
               />
             ))}
