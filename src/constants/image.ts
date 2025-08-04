26e397da92866c82d6ded25c7c92d87e89b2d6ca
Seio924 <134473627+Seio924@users.noreply.github.com>
2025-08-04 23:09:22 +0900

    [FEAT] 개발, 스테이징 서버 로고 변경 #298 (#299)
    * feat: 환경변수에 따른 로고 이미지 조건 분기 처리
    
    * refactor: 로고 매핑 상수로 리팩토링
    
    * refactor: DeployEnv type 위치 이동

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,45 +1,56 @@
-export const DEFAULT_AVATAR_IMAGE = 'https://cdn.cogroom.com/default_image/Profile.png' as const;
-export const DEFAULT_THUMBNAIL = 'https://cdn.cogroom.com/default_image/Thumbnail.png' as const;
-export const REPORT_IMAGE = 'https://cdn.cogroom.com/default_image/Report.png' as const;
+import { DeployEnv } from '@/types/common';
+
+// Avatar
+export const DEFAULT_AVATAR_IMAGE: string = 'https://cdn.cogroom.com/default_image/Profile.png';
+export const DEFAULT_THUMBNAIL: string = 'https://cdn.cogroom.com/default_image/Thumbnail.png';
+export const REPORT_IMAGE: string = 'https://cdn.cogroom.com/default_image/Report.png';
 
 // Logo
-export const DEFAULT_LOGO_SYMBOL_NORMAL = 'https://cdn.cogroom.com/default_image/Logo/Logo_Symbol_normal.svg' as const;
-export const DEFAULT_LOGO_SYMBOL_GRAY = 'https://cdn.cogroom.com/default_image/Logo/Logo_Symbol_gray.svg' as const;
-export const DEFAULT_LOGO_TYPE_NORMAL = 'https://cdn.cogroom.com/default_image/Logo/Logo_Type_normal.svg' as const;
-export const DEFAULT_LOGO_HORIZONTAL_NORMAL =
-  'https://cdn.cogroom.com/default_image/Logo/Logo_Horizontal_normal.svg' as const;
-export const DEFAULT_LOGO_HORIZONTAL_WHITE =
-  'https://cdn.cogroom.com/default_image/Logo/Logo_Horizontal_white.svg' as const;
-export const DEFAULT_LOGO_HORIZONTAL_BLACK =
-  'https://cdn.cogroom.com/default_image/Logo/Logo_Horizontal_black.svg' as const;
-export const DEFAULT_LOGO_VERTICAL_NORMAL =
-  'https://cdn.cogroom.com/default_image/Logo/Logo_Vertical_normal.svg' as const;
-export const DEFAULT_LOGO_VERTICAL_WHITE =
-  'https://cdn.cogroom.com/default_image/Logo/Logo_Vertical_white.svg' as const;
-export const DEFAULT_LOGO_VERTICAL_BLACK =
-  'https://cdn.cogroom.com/default_image/Logo/Logo_Vertical_black.svg' as const;
-export const DEFAULT_LOGO_KOREAN = 'https://cdn.cogroom.com/default_image/Logo/Logo_Korean.svg' as const;
+export const DEFAULT_LOGO_SYMBOL_NORMAL: string = 'https://cdn.cogroom.com/default_image/Logo/Logo_Symbol_normal.svg';
+export const DEFAULT_LOGO_SYMBOL_GRAY: string = 'https://cdn.cogroom.com/default_image/Logo/Logo_Symbol_gray.svg';
+export const DEFAULT_LOGO_TYPE_NORMAL: string = 'https://cdn.cogroom.com/default_image/Logo/Logo_Type_normal.svg';
+export const DEFAULT_LOGO_HORIZONTAL_NORMAL: string =
+  'https://cdn.cogroom.com/default_image/Logo/Logo_Horizontal_normal.svg';
+export const DEFAULT_LOGO_HORIZONTAL_WHITE: string =
+  'https://cdn.cogroom.com/default_image/Logo/Logo_Horizontal_white.svg';
+export const DEFAULT_LOGO_HORIZONTAL_BLACK: string =
+  'https://cdn.cogroom.com/default_image/Logo/Logo_Horizontal_black.svg';
+export const DEFAULT_LOGO_VERTICAL_NORMAL: string =
+  'https://cdn.cogroom.com/default_image/Logo/Logo_Vertical_normal.svg';
+export const DEFAULT_LOGO_VERTICAL_WHITE: string = 'https://cdn.cogroom.com/default_image/Logo/Logo_Vertical_white.svg';
+export const DEFAULT_LOGO_VERTICAL_BLACK: string = 'https://cdn.cogroom.com/default_image/Logo/Logo_Vertical_black.svg';
+export const DEFAULT_LOGO_KOREAN: string = 'https://cdn.cogroom.com/default_image/Logo/Logo_Korean.svg';
+export const DEFAULT_LOGO_STAGING: string = 'https://cdn.cogroom.com/default_image/Logo/Logo_Staging.svg';
+export const DEFAULT_LOGO_DEV: string = 'https://cdn.cogroom.com/default_image/Logo/Logo_Dev.svg';
+
+export const ENV_LOGO_MAP: Record<DeployEnv, string> = {
+  production: DEFAULT_LOGO_HORIZONTAL_NORMAL,
+  staging: DEFAULT_LOGO_STAGING,
+  development: DEFAULT_LOGO_DEV,
+};
 
 // Home
-export const DEFAULT_HERO_WAVY = 'https://cdn.cogroom.com/default_image/Hero_Wavy.gif' as const;
-export const DEFAULT_DAILY_WATERDROP = 'https://cdn.cogroom.com/default_image/Daily_Waterdrop.png' as const;
-export const DEFAULT_COMMUNITY_BANNER = 'https://cdn.cogroom.com/default_image/Community_Banner.png' as const;
+export const DEFAULT_HERO_WAVY: string = 'https://cdn.cogroom.com/default_image/Hero_Wavy.gif';
+export const DEFAULT_DAILY_WATERDROP: string = 'https://cdn.cogroom.com/default_image/Daily_Waterdrop.png';
+export const DEFAULT_COMMUNITY_BANNER: string = 'https://cdn.cogroom.com/default_image/Community_Banner.png';
 
 // Daily
-export const DEFAULT_QUESTION_BACKGROUND =
-  'https://cdn.cogroom.com/default_image/Daily_Question_Background.png' as const;
-export const DEFAULT_WATERDROP = 'https://cdn.cogroom.com/default_image/Waterdrop.svg' as const;
+export const DEFAULT_QUESTION_BACKGROUND: string =
+  'https://cdn.cogroom.com/default_image/Daily_Question_Background.png';
+export const DEFAULT_WATERDROP: string = 'https://cdn.cogroom.com/default_image/Waterdrop.svg';
 
 // Contents
-export const DEFAULT_CONTENT_MAIN_BANNER = 'https://cdn.cogroom.com/default_image/Content_Main_Banner.png' as const;
+export const DEFAULT_CONTENT_MAIN_BANNER: string = 'https://cdn.cogroom.com/default_image/Content_Main_Banner.png';
 
 // Community
-export const DEFAULT_COMMUNITY_MAIN_BANNER = 'https://cdn.cogroom.com/default_image/Community_Main_Banner.png' as const;
+export const DEFAULT_COMMUNITY_MAIN_BANNER: string = 'https://cdn.cogroom.com/default_image/Community_Main_Banner.png';
 
-export const STREAK_SHARE_IMAGE_URLS = [
+// Loading
+export const DEFAULT_LOADING: string = 'https://cdn.cogroom.com/default_image/Loading.png';
+
+// Streak share
+export const STREAK_SHARE_IMAGE_URLS: string[] = [
   'https://cdn.cogroom.com/default_image/StreakShare01.png',
   'https://cdn.cogroom.com/default_image/StreakShare02.png',
   'https://cdn.cogroom.com/default_image/StreakShare03.png',
 ];
-// Loading
-export const DEFAULT_LOADING = 'https://cdn.cogroom.com/default_image/Loading.png' as const;
--- src/types/common.ts
+++ src/types/common.ts
@@ -2,3 +2,5 @@ export interface SelectOption {
   label: string;
   value: string | number;
 }
+
+export type DeployEnv = 'production' | 'staging' | 'development';
