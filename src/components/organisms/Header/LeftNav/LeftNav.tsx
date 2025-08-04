e91ebceebc12abbd83c11f9d2666a2997d77d8b2
Seio924 <seio9241@gmail.com>
2025-08-04 15:03:25 +0900

    feat: 환경변수에 따른 로고 이미지 조건 분기 처리

--- src/components/organisms/Header/LeftNav/LeftNav.tsx
+++ src/components/organisms/Header/LeftNav/LeftNav.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import { DEFAULT_LOGO_HORIZONTAL_NORMAL } from '@/constants/image';
+import { DEFAULT_LOGO_HORIZONTAL_NORMAL, DEFAULT_LOGO_STAGING, DEFAULT_LOGO_DEV } from '@/constants/image';
 
 import * as S from './LeftNav.styled';
 import NavList from '../NavList/NavList';
@@ -12,11 +12,20 @@ import NavList from '../NavList/NavList';
 export default function LeftNav() {
   const pathname = usePathname();
 
+  const env = process.env.NEXT_PUBLIC_DEPLOY_ENV;
+  let logoSrc = DEFAULT_LOGO_HORIZONTAL_NORMAL;
+
+  if (env === 'staging') {
+    logoSrc = DEFAULT_LOGO_STAGING;
+  } else if (env === 'development') {
+    logoSrc = DEFAULT_LOGO_DEV;
+  }
+
   return (
     <S.LeftNav>
       <Link href='/'>
         <Image
-          src={DEFAULT_LOGO_HORIZONTAL_NORMAL}
+          src={logoSrc}
           alt='Logo'
           width={120}
           height={23}
