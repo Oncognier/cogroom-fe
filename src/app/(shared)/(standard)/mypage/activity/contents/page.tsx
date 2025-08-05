394e1617ed293c2ee7b90dc18e74afd46028748c
Leesugyoung <tnrud4685@naver.com>
2025-08-05 21:55:07 +0900

    feat: 마이페이지 ui 개선건

--- src/app/(shared)/(standard)/mypage/activity/contents/page.tsx
+++ src/app/(shared)/(standard)/mypage/activity/contents/page.tsx
@@ -2,5 +2,10 @@ import ScriptX from '@/assets/icons/script-x.svg';
 import EmptyState from '@/components/organisms/EmptyState/EmptyState';
 
 export default function Contents() {
-  return <EmptyState icon={<ScriptX />} hideMessage />;
+  return (
+    <EmptyState
+      icon={<ScriptX />}
+      hideMessage
+    />
+  );
 }
