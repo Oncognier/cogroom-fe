679ff619ff528cc707ee71791ac17f1a6feace37
LEE SU-GYEONG <tnrud4685@naver.com>
2025-08-09 11:16:17 +0900

    feat: 어드민가드 안내문구 수정 (#319)

--- src/app/(shared)/(standard)/adminguard/page.tsx
+++ src/app/(shared)/(standard)/adminguard/page.tsx
@@ -5,7 +5,7 @@ import * as S from './page.styled';
 export default function AdminGuard() {
   return (
     <S.AdminGuard>
-      <S.MainMessage>해당 기능은 권한이 있는 사용자만 이용할 수 있어요!</S.MainMessage>
+      <S.MainMessage>권한이 없어요!</S.MainMessage>
     </S.AdminGuard>
   );
 }
