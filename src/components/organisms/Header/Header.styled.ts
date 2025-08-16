95e4dcb56cbb5b150b565151df49a506ccb2bdbe
Seio924 <seio9241@gmail.com>
2025-08-17 02:35:00 +0900

    feat: Header 반응형 설정

--- src/components/organisms/Header/Header.styled.ts
+++ src/components/organisms/Header/Header.styled.ts
@@ -1,22 +1,50 @@
 'use client';
 
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-export const Header = styled.header`
+import { mqMin } from '@/styles/foundation';
+
+const container = css`
   width: 100%;
-  height: 6.9rem;
-
-  background-color: ${({ theme }) => theme.semantic.static.white};
-  border-bottom: 1px solid ${({ theme }) => theme.semantic.line.normal};
-`;
-
-export const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  height: 100%;
   max-width: 1100px;
   padding: 0 2rem;
   margin: 0 auto;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const HeaderBar = styled.div`
+  width: 100%;
+  background-color: ${({ theme }) => theme.semantic.static.white};
+  border-bottom: 1px solid ${({ theme }) => theme.semantic.line.normal};
+
+  ${mqMin.desktop} {
+    &:last-child {
+      border-bottom: none;
+    }
+  }
+`;
+
+export const HeaderInner = styled.div`
+  ${container};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 6.9rem;
+`;
+
+export const SmallScreenNav = styled.nav`
+  ${container};
+  display: flex;
+  align-items: center;
+  height: 4.2rem;
+
+  ${mqMin.desktop} {
+    display: none;
+  }
 `;
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,14 +1,23 @@
 import * as S from './Header.styled';
 import LeftNav from './LeftNav/LeftNav';
+import NavList from './NavList/NavList';
 import RightNav from './RightNav/RightNav';
 
 export default async function Header() {
   return (
     <S.Header>
-      <S.Wrapper>
-        <LeftNav />
-        <RightNav />
-      </S.Wrapper>
+      <S.HeaderBar>
+        <S.HeaderInner>
+          <LeftNav />
+          <RightNav />
+        </S.HeaderInner>
+      </S.HeaderBar>
+
+      <S.HeaderBar>
+        <S.SmallScreenNav>
+          <NavList />
+        </S.SmallScreenNav>
+      </S.HeaderBar>
     </S.Header>
   );
 }
