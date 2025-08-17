5c35ef84a64e90badfe5573c857c9fcb92582b72
Seio924 <134473627+Seio924@users.noreply.github.com>
2025-08-17 13:28:19 +0900

    [FEAT] Header 컴포넌트 반응형 적용 #350 (#367)
    * refactor: breakpoints 설정 리팩토링
    
    * feat: LeftNav의 NavList 반응형 설정
    
    * feat: Header 반응형 설정
    
    * refactor: div 네이밍 명확하도록 변경
    
    * refactor: Header 리팩토링

--- src/components/organisms/Header/LeftNav/LeftNav.styled.ts
+++ src/components/organisms/Header/LeftNav/LeftNav.styled.ts
@@ -2,8 +2,16 @@
 
 import styled from '@emotion/styled';
 
+import { mqMax } from '@/styles/foundation';
+
 export const LeftNav = styled.div`
   display: flex;
   align-items: center;
   gap: 4rem;
+`;
+
+export const DesktopContainer = styled.div`
+  ${mqMax.desktop} {
+    display: none;
+  }
 `;
