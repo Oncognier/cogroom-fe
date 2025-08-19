6af40aa79d021ddc3cae9c265a1abcc245dd59f9
Leesugyoung <tnrud4685@naver.com>
2025-08-19 21:24:53 +0900

    feat: 개인정보 설정 반응형 적용

--- src/app/(shared)/(standard)/mypage/layout.styled.ts
+++ src/app/(shared)/(standard)/mypage/layout.styled.ts
@@ -11,6 +11,10 @@ export const MyPageLayout = styled.div`
 
   ${mqMax.desktop} {
     gap: 2.7rem;
+
+    & > *:last-child {
+      margin-top: 3.3rem;
+    }
   }
 `;
 
@@ -109,6 +113,7 @@ export const MobileMenuButton = styled.button`
 
   ${mqMax.desktop} {
     display: flex;
+    max-width: 100%;
   }
 
   &:hover {
--- src/app/(shared)/(standard)/mypage/setting/_components/NicknameForm/NicknameForm.styled.ts
+++ src/app/(shared)/(standard)/mypage/setting/_components/NicknameForm/NicknameForm.styled.ts
@@ -2,12 +2,33 @@
 
 import styled from '@emotion/styled';
 
+import { mqMax, mqMin } from '@/styles/foundation';
+
 export const NicknameForm = styled.div`
   display: flex;
   align-items: flex-end;
+  width: 100%;
+
   gap: 1.6rem;
 
-  width: 100%;
+  ${mqMin.tablet} {
+    & > *:first-child {
+      max-width: 34.5rem;
+      flex: 1;
+    }
+  }
+
+  ${mqMax.tablet} {
+    display: flex;
+    align-items: flex-end;
+    width: 100%;
+
+    gap: 1.5rem;
+
+    & > *:first-child {
+      flex: 1;
+    }
+  }
 `;
 
 export const ButtonWrapper = styled.div<{ isError?: boolean }>`
