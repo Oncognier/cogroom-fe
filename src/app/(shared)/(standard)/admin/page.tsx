4d6d28e6d0dca4d35c6d1aab55bdb3ec2cc40a7a
오지훈 <gfsd2397@gmail.com>
2025-08-20 22:24:29 +0900

    feat: admin 첫 페이지 contents 로 변경

--- src/app/(shared)/(standard)/admin/page.tsx
+++ src/app/(shared)/(standard)/admin/page.tsx
@@ -2,19 +2,6 @@
 
 import { redirect } from 'next/navigation';
 
-import Loading from '@/components/organisms/Loading/Loading';
-import useGetUserSummary from '@/hooks/api/member/useGetUserSummary';
-
 export default function Admin() {
-  const { data, isLoading } = useGetUserSummary();
-
-  const role = data?.memberRole;
-
-  if (isLoading) return <Loading />;
-
-  if (role === 'CONTENT_PROVIDER') {
-    redirect('/admin/contents');
-  }
-
-  redirect('/admin/notices');
+  redirect('/admin/contents');
 }
