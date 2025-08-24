94c0ec5434d26023469b4074aca6ed1c02607107
오지훈 <gfsd2397@gmail.com>
2025-08-25 01:22:31 +0900

    feat: mypage comments EmptyState 적용

---
+++ src/app/(shared)/(standard)/mypage/community/comments/page.styled.ts
@@ -0,0 +1,15 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+`;
+
+export const Title = styled.p`
+  ${({ theme }) => theme.typography.heading1.semibold};
+  color: ${({ theme }) => theme.semantic.label.normal};
+`;
--- src/app/(shared)/(standard)/mypage/community/comments/page.tsx
+++ src/app/(shared)/(standard)/mypage/community/comments/page.tsx
@@ -1,6 +1,35 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import ArrowRight from '@/assets/icons/arrowright.svg';
 import MessageCircleX from '@/assets/icons/message-circle-x.svg';
+import SolidButton from '@/components/atoms/SolidButton/SolidButton';
 import EmptyState from '@/components/organisms/EmptyState/EmptyState';
 
+import * as S from './page.styled';
+
 export default function Comments() {
-  return <EmptyState icon={<MessageCircleX />} />;
+  const router = useRouter();
+
+  const handleGoToCommunity = () => {
+    router.push('/community');
+  };
+
+  return (
+    <EmptyState icon={<MessageCircleX />}>
+      <S.Wrapper>
+        <S.Title>다른 코그니어 글에 댓글을 달아봐요</S.Title>
+        <SolidButton
+          size='sm'
+          color='primary'
+          label='댓글 달러가기'
+          interactionVariant='normal'
+          onClick={handleGoToCommunity}
+          iconRight={<ArrowRight />}
+          type='button'
+        />
+      </S.Wrapper>
+    </EmptyState>
+  );
 }
