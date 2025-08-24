2fc3dd1ced699bcd44f49bf4a10c1f0156076cb3
오지훈 <gfsd2397@gmail.com>
2025-08-25 01:22:09 +0900

    feat: mypage posts EmptyState 적용

--- src/app/(shared)/(standard)/mypage/community/posts/page.tsx
+++ src/app/(shared)/(standard)/mypage/community/posts/page.tsx
@@ -1,6 +1,35 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import ArrowRight from '@/assets/icons/arrowright.svg';
 import MessageCircleX from '@/assets/icons/message-circle-x.svg';
+import SolidButton from '@/components/atoms/SolidButton/SolidButton';
 import EmptyState from '@/components/organisms/EmptyState/EmptyState';
 
+import * as S from './page.styled';
+
 export default function Posts() {
-  return <EmptyState icon={<MessageCircleX />} />;
+  const router = useRouter();
+
+  const handleGoToCommunity = () => {
+    router.push('/community');
+  };
+
+  return (
+    <EmptyState icon={<MessageCircleX />}>
+      <S.Wrapper>
+        <S.Title>코그니어 커뮤니티에 첫 글을 써 보세요!</S.Title>
+        <SolidButton
+          size='sm'
+          color='primary'
+          label='커뮤니티 바로가기'
+          interactionVariant='normal'
+          onClick={handleGoToCommunity}
+          iconRight={<ArrowRight />}
+          type='button'
+        />
+      </S.Wrapper>
+    </EmptyState>
+  );
 }
