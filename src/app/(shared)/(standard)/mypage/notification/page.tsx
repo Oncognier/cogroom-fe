4f514dea8e89427119285d84f6116bf4d3e78c1b
오지훈 <gfsd2397@gmail.com>
2025-08-28 02:38:48 +0900

    feat: mypage setting 수정

--- src/app/(shared)/(standard)/mypage/notification/page.tsx
+++ src/app/(shared)/(standard)/mypage/notification/page.tsx
@@ -2,21 +2,28 @@
 
 import { useState } from 'react';
 
+import SelectAlarm from './_components/SelectTime/SelectTime';
 import SettingGroup from './_components/SettingGroup/SettingGroup';
 import SettingItem from './_components/SettingGroup/SettingItem/SettingItem';
 import * as S from './page.styled';
 
 export default function Notification() {
   const [settings, setSettings] = useState({
-    push: false,
-    sound: false,
-    news: false,
-    streak: false,
-    marketing: false,
+    pushEnabled: false,
+    soundEnabled: false,
+    importantNewsEnabled: false,
+    streakReminderEnabled: false,
+    streakReminderTime: '23:30',
+    marketingAgreementEnabled: false,
   });
 
   const toggle = (key: keyof typeof settings) => {
     setSettings((prev) => ({ ...prev, [key]: !prev[key] }));
+  };
+
+  // TODO: 추후 데일리 리마인드 api 연결
+  const handleSelectStreakReminderTime = (hour: string, minute: string) => {
+    setSettings((prev) => ({ ...prev, streakReminderTime: `${hour}:${minute}` }));
   };
 
   return (
@@ -24,32 +31,37 @@ export default function Notification() {
       <SettingGroup title='사이트 알림'>
         <SettingItem
           label='푸시 알림'
-          isActive={settings.push}
-          onChange={() => toggle('push')}
+          isActive={settings.pushEnabled}
+          onChange={() => toggle('pushEnabled')}
         />
         <SettingItem
           label='소리 알림'
-          isActive={settings.sound}
-          onChange={() => toggle('sound')}
+          isActive={settings.soundEnabled}
+          onChange={() => toggle('soundEnabled')}
         />
       </SettingGroup>
 
       <SettingGroup title='카카오톡 알림'>
         <SettingItem
           label='중요한 소식 알림'
-          isActive={settings.news}
-          onChange={() => toggle('news')}
+          isActive={settings.importantNewsEnabled}
+          onChange={() => toggle('importantNewsEnabled')}
         />
         <SettingItem
           label='스트릭 리마인드 알림'
-          description='시간 설정 10:00PM'
-          isActive={settings.streak}
-          onChange={() => toggle('streak')}
-        />
+          isActive={settings.streakReminderEnabled}
+          onChange={() => toggle('streakReminderEnabled')}
+        >
+          <SelectAlarm
+            streakTime={settings.streakReminderTime}
+            disabled={!settings.streakReminderEnabled}
+            onSelect={handleSelectStreakReminderTime}
+          />
+        </SettingItem>
         <SettingItem
           label='마케팅 수신 동의'
-          isActive={settings.marketing}
-          onChange={() => toggle('marketing')}
+          isActive={settings.marketingAgreementEnabled}
+          onChange={() => toggle('marketingAgreementEnabled')}
         />
       </SettingGroup>
     </S.NotificationContainer>
