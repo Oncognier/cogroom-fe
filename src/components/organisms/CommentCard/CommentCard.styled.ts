4d6a9c4ca5b16444676814ed3f68be2c0a431208
오지훈 <gfsd2397@gmail.com>
2025-08-31 00:38:06 +0900

    feat: Comment Card component

---
+++ src/components/organisms/CommentCard/CommentCard.styled.ts
@@ -0,0 +1,70 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const CommentCard = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  gap: 2.2rem;
+
+  width: 100%;
+  padding: 2.4rem 3.2rem;
+`;
+
+export const CommentRight = styled.div`
+  display: flex;
+  width: 29.6rem;
+  gap: 1.2rem;
+
+  color: ${({ theme }) => theme.semantic.label.normal};
+`;
+
+export const Icon = styled.div`
+  width: 2.4rem;
+  height: 2.4rem;
+`;
+
+export const Comment = styled.p`
+  ${({ theme }) => theme.typography.body2.medium};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const CommentLeft = styled.div`
+  display: flex;
+  align-items: center;
+  width: 40.4rem;
+
+  gap: 0.8rem;
+
+  color: ${({ theme }) => theme.semantic.label.alternative};
+`;
+
+export const Post = styled.div`
+  display: flex;
+  align-items: center;
+
+  gap: 0.8rem;
+`;
+
+export const PostIcon = styled.div`
+  width: 1.8rem;
+  height: 1.8rem;
+`;
+
+export const PostTitle = styled.p`
+  width: 25rem;
+  ${({ theme }) => theme.typography.label1.regular};
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const CommentCreatedAt = styled.p`
+  width: 12rem;
+  ${({ theme }) => theme.typography.label1.regular};
+  text-align: center;
+`;
---
+++ src/components/organisms/CommentCard/CommentCard.tsx
@@ -0,0 +1,45 @@
+import { useRouter } from 'next/navigation';
+
+import ArrowTurnDownRight from '@/assets/icons/arrowturndownright.svg';
+import type { Comment } from '@/types/post';
+import { formatDayAsSlashYYMMDD } from '@/utils/date/formatDay';
+
+import * as S from './CommentCard.styled';
+
+type CommentCardProps = {
+  commentData: Comment;
+};
+
+export default function CommentCard({ commentData }: CommentCardProps) {
+  const router = useRouter();
+
+  const { comment, parentId, post, createdAt } = commentData;
+
+  // TODO: CommentCard onClick
+  const handleClick = () => {
+    router.push(`/post/${post.postId}`);
+  };
+
+  return (
+    <S.CommentCard>
+      <S.CommentRight>
+        {parentId && (
+          <S.Icon>
+            <ArrowTurnDownRight />
+          </S.Icon>
+        )}
+        <S.Comment>{comment}</S.Comment>
+      </S.CommentRight>
+
+      <S.CommentLeft>
+        <S.Post>
+          <S.PostIcon>
+            <ArrowTurnDownRight />
+          </S.PostIcon>
+          <S.PostTitle>{post.title}</S.PostTitle>
+        </S.Post>
+        <S.CommentCreatedAt>{formatDayAsSlashYYMMDD(createdAt)}</S.CommentCreatedAt>
+      </S.CommentLeft>
+    </S.CommentCard>
+  );
+}
