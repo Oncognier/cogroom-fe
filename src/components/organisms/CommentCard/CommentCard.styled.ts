34eff91b54a1979125558a3fd299e74ce31bd919
오지훈 <gfsd2397@gmail.com>
2025-08-31 01:01:23 +0900

    fix: Commentright color 설정 수정 및 스타일 순서 정리

--- src/components/organisms/CommentCard/CommentCard.styled.ts
+++ src/components/organisms/CommentCard/CommentCard.styled.ts
@@ -5,7 +5,6 @@ import styled from '@emotion/styled';
 export const CommentCard = styled.div`
   display: flex;
   justify-content: space-between;
-
   gap: 2.2rem;
 
   width: 100%;
@@ -15,8 +14,9 @@ export const CommentCard = styled.div`
 export const CommentLeft = styled.div`
   display: flex;
   align-items: center;
-  max-width: 29.6rem;
   gap: 1.2rem;
+
+  max-width: 29.6rem;
 
   color: ${({ theme }) => theme.semantic.label.normal};
 `;
@@ -28,7 +28,9 @@ export const CommentIcon = styled.div`
 
 export const Comment = styled.p`
   max-width: 26rem;
+
   ${({ theme }) => theme.typography.body2.medium};
+
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -38,11 +40,9 @@ export const CommentRight = styled.div`
   display: flex;
   align-items: center;
   justify-content: end;
-  max-width: 40.4rem;
-
   gap: 0.8rem;
 
-  color: ${({ theme }) => theme.semantic.label.alternative};
+  max-width: 40.4rem;
 `;
 
 export const Post = styled.div`
@@ -59,7 +59,9 @@ export const PostIcon = styled.div`
 
 export const PostTitle = styled.p`
   max-width: 25rem;
+
   ${({ theme }) => theme.typography.label1.regular};
+  color: ${({ theme }) => theme.semantic.label.alternative};
 
   white-space: nowrap;
   overflow: hidden;
@@ -68,6 +70,7 @@ export const PostTitle = styled.p`
 
 export const CommentCreatedAt = styled.p`
   width: 12rem;
+
   ${({ theme }) => theme.typography.label1.regular};
-  text-align: center;
+  color: ${({ theme }) => theme.semantic.label.alternative};
 `;
