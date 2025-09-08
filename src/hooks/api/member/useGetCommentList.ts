fb71d55fff9a9968cd15b727eba2476ab62158dd
오지훈 <gfsd2397@gmail.com>
2025-09-09 03:21:45 +0900

    refactor: 마이페이지 - 댓글 훅 params type 수정

--- src/hooks/api/member/useGetCommentList.ts
+++ src/hooks/api/member/useGetCommentList.ts
@@ -2,9 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 
 import { memberApi } from '@/api/memberApis';
 import { MEMBER_QUERY_KEYS } from '@/constants/queryKeys';
-import { UserCommentListRequest } from '@/types/member';
+import { UserCommunityRequest } from '@/types/member';
 
-export default function useGetCommentList(params: UserCommentListRequest) {
+export default function useGetCommentList(params: UserCommunityRequest) {
   return useQuery({
     queryKey: [...MEMBER_QUERY_KEYS.MEMBER_COMMENTS, params],
     queryFn: () => memberApi.getCommentList(params),
