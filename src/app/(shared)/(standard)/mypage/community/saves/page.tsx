d5c3c865a0c07114e9237a5f81c85382ad56b259
오지훈 <gfsd2397@gmail.com>
2025-09-15 02:39:18 +0900

    feat: mypage - saves

--- src/app/(shared)/(standard)/mypage/community/saves/page.tsx
+++ src/app/(shared)/(standard)/mypage/community/saves/page.tsx
@@ -1,23 +1,121 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
+import SortButton from '@/app/(shared)/(standard)/mypage/_components/SortButton/SortButton';
 import MessageCircleX from '@/assets/icons/message-circle-x.svg';
+import NumberPagination from '@/components/molecules/NumberPagination/NumberPagination';
+import SearchFilter from '@/components/molecules/SearchFilter/SearchFilter';
 import EmptyState from '@/components/organisms/EmptyState/EmptyState';
+import Loading from '@/components/organisms/Loading/Loading';
+import PostCard from '@/components/organisms/PostCard/PostCard';
+import { CATEGORY_SELECT_OPTIONS } from '@/constants/common';
+import useGetUserSaveList from '@/hooks/api/member/useGetUserSaveList';
+import { useUrlSearchParams } from '@/hooks/useUrlSearchParams';
+import { SortType } from '@/types/member';
+import { formatDayAsDashYYYYMMDD } from '@/utils/date/formatDay';
+
+import * as S from './page.styled';
 
 export default function Saves() {
   const router = useRouter();
+  const { updateSearchParams, getSearchParam, getSearchParamAsDate, getSearchParamAsArray } = useUrlSearchParams();
+  const [sort, setSort] = useState<SortType>('latest');
+  const [currentPage, setCurrentPage] = useState(Number(getSearchParam('page') ?? 0));
+
+  const { data: UserSaveData, isLoading } = useGetUserSaveList({
+    page: currentPage,
+    sort,
+    category: getSearchParamAsArray('category').map(Number).filter(Boolean),
+    keyword: getSearchParam('keyword') ?? '',
+    startDate: formatDayAsDashYYYYMMDD(getSearchParamAsDate('startDate')),
+    endDate: formatDayAsDashYYYYMMDD(getSearchParamAsDate('endDate')),
+  });
 
   const handleGoToCommunity = () => {
     router.push('/community');
   };
 
+  const totalPages = UserSaveData?.totalPages ?? 1;
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    updateSearchParams({ page: page + 1 });
+  };
+
+  const handleSortChange = () => {
+    const newSort = sort === 'latest' ? 'oldest' : 'latest';
+    setSort(newSort);
+    updateSearchParams({ sort: newSort });
+  };
+
+  const urlPageNum = Number(getSearchParam('page') ?? 0);
+
+  useEffect(() => {
+    if (urlPageNum > 0) {
+      setCurrentPage(urlPageNum - 1);
+    }
+  }, [urlPageNum]);
+
+  if (isLoading) return <Loading />;
+
+  if (!UserSaveData)
+    return (
+      <EmptyState
+        icon={<MessageCircleX />}
+        description='꼭 마음에 담아두고 싶던 글이 있나요?'
+        buttonLabel='글 보러가기'
+        buttonAction={handleGoToCommunity}
+      />
+    );
+
   return (
-    <EmptyState
-      icon={<MessageCircleX />}
-      description='꼭 마음에 담아두고 싶던 글이 있나요?'
-      buttonLabel='글 보러가기'
-      buttonAction={handleGoToCommunity}
-    />
+    <S.UserSave>
+      <S.FilterHeader>
+        <SearchFilter
+          totalTitle='전체 댓글'
+          total={UserSaveData?.totalElements}
+          fields={{
+            dateRange: {},
+            select: [
+              {
+                name: 'category',
+                placeholder: '카테고리 선택',
+                options: CATEGORY_SELECT_OPTIONS,
+                isMulti: true,
+              },
+            ],
+            search: { placeholder: '댓글 내용 입력' },
+          }}
+          actions={[{ type: 'submit', label: '검색하기' }]}
+        />
+
+        <S.SortButtonWrapper>
+          <SortButton
+            sort={sort}
+            onClick={handleSortChange}
+          />
+        </S.SortButtonWrapper>
+      </S.FilterHeader>
+
+      <S.SaveList>
+        {UserSaveData?.data.map((post) => (
+          <PostCard
+            key={post.postId}
+            post={post}
+          />
+        ))}
+      </S.SaveList>
+
+      <S.Pagination>
+        <NumberPagination
+          size='nm'
+          currentPage={currentPage + 1}
+          totalPages={totalPages}
+          onPageChange={(page) => handlePageChange(page - 1)}
+        />
+      </S.Pagination>
+    </S.UserSave>
   );
 }
