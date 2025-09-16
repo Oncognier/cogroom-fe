262e945bf2334bf6589ddc2d640d8a09b82bc404
오지훈 <gfsd2397@gmail.com>
2025-09-16 10:21:00 +0900

    refactor: type params 수정

--- src/hooks/api/member/useGetUserSaveList.ts
+++ src/hooks/api/member/useGetUserSaveList.ts
@@ -2,11 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 
 import { memberApi } from '@/api/memberApis';
 import { MEMBER_QUERY_KEYS } from '@/constants/queryKeys';
-import { UserPostRequest } from '@/types/member';
+import { UserCommunityRequest } from '@/types/member';
 
-export default function useGetUserSaveList(params: UserPostRequest) {
+export default function useGetUserSaveList(params: UserCommunityRequest) {
   return useQuery({
     queryKey: [...MEMBER_QUERY_KEYS.MEMBER_SAVES, params],
-    queryFn: () => memberApi.getSaveList(params),
+    queryFn: () => memberApi.getUserSaveList(params),
   });
 }
