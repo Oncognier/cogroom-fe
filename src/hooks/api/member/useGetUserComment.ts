bfdb4adc203daf88086afda5a6abd7e22f671804
오지훈 <gfsd2397@gmail.com>
2025-09-16 11:25:33 +0900

    refactor: member community 네이밍 List 제거

--- src/hooks/api/member/useGetUserComment.ts
+++ src/hooks/api/member/useGetUserComment.ts
@@ -4,9 +4,9 @@ import { memberApi } from '@/api/memberApis';
 import { MEMBER_QUERY_KEYS } from '@/constants/queryKeys';
 import { UserCommunityRequest } from '@/types/member';
 
-export default function useGetUserPostList(params: UserCommunityRequest) {
+export default function useGetUserComment(params: UserCommunityRequest) {
   return useQuery({
-    queryKey: [...MEMBER_QUERY_KEYS.MEMBER_POSTS, params],
-    queryFn: () => memberApi.getUserPostList(params),
+    queryKey: [...MEMBER_QUERY_KEYS.MEMBER_COMMENTS, params],
+    queryFn: () => memberApi.getUserComment(params),
   });
 }
--- src/hooks/api/member/useGetUserSave.ts
+++ src/hooks/api/member/useGetUserSave.ts
@@ -4,9 +4,9 @@ import { memberApi } from '@/api/memberApis';
 import { MEMBER_QUERY_KEYS } from '@/constants/queryKeys';
 import { UserCommunityRequest } from '@/types/member';
 
-export default function useGetUserSaveList(params: UserCommunityRequest) {
+export default function useGetUserSave(params: UserCommunityRequest) {
   return useQuery({
     queryKey: [...MEMBER_QUERY_KEYS.MEMBER_SAVES, params],
-    queryFn: () => memberApi.getUserSaveList(params),
+    queryFn: () => memberApi.getUserSave(params),
   });
 }
