2aa610f94dc8ec79593b3e83843a48a80b19f224
오지훈 <gfsd2397@gmail.com>
2025-09-18 01:58:29 +0900

    feat: mypage - likes post ui

---
+++ src/app/(shared)/(standard)/mypage/community/likes/posts/page.styled.ts
@@ -0,0 +1,42 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const UserSave = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+`;
+
+export const FilterHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
+
+export const ListControlsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  height: 4.2rem;
+`;
+
+export const SwitchLikeButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+`;
+
+export const SaveList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+`;
+
+export const Pagination = styled.div`
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+  padding: 2.4rem 0;
+`;
