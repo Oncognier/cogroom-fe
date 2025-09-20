834fbf8c8bdae3f96a3615ccf0efd07a9b8a1e5a
Seio924 <seio9241@gmail.com>
2025-09-20 14:54:01 +0900

    feat: NavItem에 스켈레톤 추가

--- src/components/organisms/Header/NavItem/NavItem.tsx
+++ src/components/organisms/Header/NavItem/NavItem.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 
 import * as S from './NavItem.styled';
 import type { NavItemStyleProps } from './NavItem.styled';
+import { useAuthStore } from '@/stores/useAuthStore';
+import Skeleton from '@/components/skeleton/Skeleton/Skeleton';
 
 interface NavItemProps extends NavItemStyleProps {
   label: string;
@@ -9,6 +11,18 @@ interface NavItemProps extends NavItemStyleProps {
 }
 
 export default function NavItem({ label, href, isActive }: NavItemProps) {
+  const status = useAuthStore((s) => s.status);
+
+  if (status === 'unknown') {
+    return (
+      <Skeleton
+        width='4rem'
+        height='2.3rem'
+        borderRadius='0.6rem'
+      />
+    );
+  }
+
   return (
     <S.NavItem isActive={isActive}>
       <Link href={href}>{label}</Link>
