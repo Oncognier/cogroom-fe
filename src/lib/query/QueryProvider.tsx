03cc24afceffa98a68d3c8de696b3e7ebd21c66f
Seio924 <seio9241@gmail.com>
2025-09-22 12:19:53 +0900

    fix: QueryClient 설정 수정

--- src/lib/query/QueryProvider.tsx
+++ src/lib/query/QueryProvider.tsx
@@ -2,24 +2,47 @@
 
 import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { isAxiosError } from 'axios';
 
 import { HTTPError } from '@/api/axios/errors/HTTPError';
 
-function makeQueryClient() {
+function isNetworkError(err: unknown) {
+  return isAxiosError(err) && !err.response;
+}
+
+export function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
         retry: (failureCount, error) => {
           if (error instanceof HTTPError) {
-            if (error.statusCode === 401) return false;
+            if (error.statusCode === 401 || error.statusCode === 403) return false;
           }
+          if (isNetworkError(error)) return failureCount < 1;
           return failureCount < 2;
         },
-        throwOnError: true,
+
+        throwOnError: (err) => {
+          if (err instanceof HTTPError) {
+            return err.statusCode >= 500;
+          }
+          if (isNetworkError(err)) {
+            return true;
+          }
+          return false;
+        },
+
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
       },
+
       mutations: {
         retry: false,
-        throwOnError: true,
+        throwOnError: (err) => {
+          if (err instanceof HTTPError) return err.statusCode >= 500;
+          if (isNetworkError(err)) return true;
+          return false;
+        },
       },
     },
   });
