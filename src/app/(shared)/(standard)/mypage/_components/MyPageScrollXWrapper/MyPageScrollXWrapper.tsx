fe2217483458f2102a7cf9875b1b016291f0d08e
Seio924 <seio9241@gmail.com>
2025-09-29 13:53:01 +0900

    feat: MyPageScrollXWrapper 제거

--- src/app/(shared)/(standard)/mypage/_components/MyPageScrollXWrapper/MyPageScrollXWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './MyPageScrollXWrapper.styled';
-
-export default function MyPageScrollXWrapper({ children }: { children: React.ReactNode }) {
-  return (
-    <S.MyPageScrollXWrapper>
-      <S.MyPageScrollXContent>{children}</S.MyPageScrollXContent>
-    </S.MyPageScrollXWrapper>
-  );
-}
