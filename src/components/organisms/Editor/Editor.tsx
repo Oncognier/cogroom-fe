f45f0369954ed23bf07b3e0f4869daaffaf2efc2
Leesugyoung <tnrud4685@naver.com>
2025-10-14 00:10:56 +0900

    refactor: useCustomEditor 커스텀 훅으로 분리

--- src/components/organisms/Editor/Editor.tsx
+++ src/components/organisms/Editor/Editor.tsx
@@ -1,22 +1,9 @@
 'use client';
 
-import CharacterCount from '@tiptap/extension-character-count';
-import Color from '@tiptap/extension-color';
-import { FileHandler } from '@tiptap/extension-file-handler';
-import FontFamily from '@tiptap/extension-font-family';
-import Placeholder from '@tiptap/extension-placeholder';
-import { TextAlign } from '@tiptap/extension-text-align';
-import { TextStyle } from '@tiptap/extension-text-style';
-import { Underline } from '@tiptap/extension-underline';
-import { useEditor, EditorContent } from '@tiptap/react';
-import { StarterKit } from '@tiptap/starter-kit';
-import { useEffect } from 'react';
+import { EditorContent } from '@tiptap/react';
 
-import { useUploadFileToS3Mutation } from '@/hooks/api/file/useUploadFileToS3';
-import { LimitListNesting } from '@/utils/editor/LimitListNesting';
+import { useCustomEditor } from '@/hooks/useCustomEditor';
 
-import { CustomImage } from './CustomImage';
-import { CustomLink } from './CustomLink';
 import CustomToolbar from './CustomToolbar/CustomToolbar';
 import * as S from './Editor.styled';
 
@@ -37,81 +24,12 @@ export default function Editor({
   readonly = false,
   className,
 }: EditorProps) {
-  const { uploadToS3 } = useUploadFileToS3Mutation({
-    onSuccess: (accessUrls) => {
-      if (accessUrls.length > 0 && editor) {
-        const url = accessUrls[0];
-
-        editor
-          .chain()
-          .focus()
-          .setCustomImage({
-            src: url,
-            'data-original-filename': url,
-          })
-          .run();
-      }
-    },
+  const editor = useCustomEditor({
+    value,
+    onChange,
+    placeholder,
+    readonly,
   });
-
-  const editor = useEditor({
-    immediatelyRender: false,
-    autofocus: true,
-    extensions: [
-      StarterKit,
-      CustomImage,
-      Color,
-      CustomLink.configure({
-        HTMLAttributes: {
-          class: 'editor-link',
-        },
-        openOnClick: false,
-        linkOnPaste: false,
-        autolink: false,
-      }),
-      Underline,
-      TextStyle,
-      TextAlign.configure({
-        types: ['heading', 'paragraph', 'customImage', 'listItem', 'bulletList', 'orderedList'],
-      }),
-      FontFamily.configure({
-        types: ['textStyle'],
-      }),
-      Placeholder.configure({
-        placeholder,
-        emptyEditorClass: 'is-editor-empty',
-      }),
-      CharacterCount.configure({
-        limit: 50000,
-        mode: 'textSize',
-      }),
-      LimitListNesting,
-      FileHandler.configure({
-        allowedMimeTypes: ['image/png', 'image/jpeg', 'image/gif', 'image/webp'],
-        onDrop: (currentEditor, files) => {
-          if (!files || files.length === 0) return;
-          const file = files[0];
-          uploadToS3({ files: [file] });
-        },
-        onPaste: (currentEditor, files) => {
-          if (!files || files.length === 0) return;
-          const file = files[0];
-          uploadToS3({ files: [file] });
-        },
-      }),
-    ],
-    content: value,
-    editable: !readonly,
-    onUpdate: ({ editor }) => {
-      onChange(editor.getHTML());
-    },
-  });
-
-  useEffect(() => {
-    if (editor && value !== editor.getHTML()) {
-      editor.commands.setContent(value);
-    }
-  }, [editor, value]);
 
   if (!editor) return null;
 
