170fabec93a9071677d1f677283b981533a3da72
Seio924 <seio9241@gmail.com>
2025-11-02 22:33:46 +0900

    feat: payment api 모킹 추가

---
+++ src/mocks/handlers/payment.ts
@@ -0,0 +1,64 @@
+import { http, HttpResponse } from 'msw';
+
+import { END_POINTS, HTTP_STATUS_CODE } from '@/constants/api';
+import type { VerifyPaymentRequest } from '@/types/payment';
+
+import { changePlanSuccess } from '../data/payment/changePlanData';
+import { getBillingKeySuccess } from '../data/payment/getBillingKeyData';
+import { getPlanInfoError, getPlanInfoSuccess } from '../data/payment/getPlanInfoData';
+import { getPlansSuccess } from '../data/payment/getPlansData';
+import { verifyPaymentError, verifyPaymentSuccess } from '../data/payment/verifyPaymentData';
+
+export const paymentHandlers = [
+  // 플랜 정보 조회
+  http.get(END_POINTS.PAYMENTS.PLAN, async ({ request }) => {
+    const url = new URL(request.url);
+    const planId = url.searchParams.get('planId');
+
+    if (!planId) {
+      return new HttpResponse(JSON.stringify(getPlanInfoError), {
+        status: HTTP_STATUS_CODE.BAD_REQUEST,
+      });
+    }
+
+    return new HttpResponse(JSON.stringify(getPlanInfoSuccess), {
+      status: HTTP_STATUS_CODE.OK,
+    });
+  }),
+
+  // 플랜 전체 목록 조회
+  http.get(END_POINTS.PAYMENTS.PLANS, async () => {
+    return new HttpResponse(JSON.stringify(getPlansSuccess), {
+      status: HTTP_STATUS_CODE.OK,
+    });
+  }),
+
+  // 빌링키 조회
+  http.get(END_POINTS.PAYMENTS.BILLING_KEY, async () => {
+    return new HttpResponse(JSON.stringify(getBillingKeySuccess), {
+      status: HTTP_STATUS_CODE.OK,
+    });
+  }),
+
+  // 플랜 변경
+  http.patch(END_POINTS.PAYMENTS.CHANGE_PLAN, async () => {
+    return new HttpResponse(JSON.stringify(changePlanSuccess), {
+      status: HTTP_STATUS_CODE.OK,
+    });
+  }),
+
+  // 결제 인증 정보 조회
+  http.post(END_POINTS.PAYMENTS.VERIFY, async ({ request }) => {
+    const body = (await request.json()) as VerifyPaymentRequest;
+
+    if (!body.identityVerificationId || !body.paymentHistoryId) {
+      return new HttpResponse(JSON.stringify(verifyPaymentError), {
+        status: HTTP_STATUS_CODE.BAD_REQUEST,
+      });
+    }
+
+    return new HttpResponse(JSON.stringify(verifyPaymentSuccess), {
+      status: HTTP_STATUS_CODE.OK,
+    });
+  }),
+];
