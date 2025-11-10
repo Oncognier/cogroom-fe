3fe0cd2eb941415bb19f8c98c0afe216d7dd352c
Seio924 <seio9241@gmail.com>
2025-11-10 17:50:24 +0900

    feat: 구독페이지에 SubscriptionCard 추가

--- src/app/(shared)/(anomaly)/subscription/page.tsx
+++ src/app/(shared)/(anomaly)/subscription/page.tsx
@@ -6,9 +6,15 @@ import { useRouter } from 'next/navigation';
 import SolidButton from '@/components/atoms/SolidButton/SolidButton';
 
 import * as S from './page.styled';
+import { useGetPlans } from '@/hooks/api/payment/useGetPlans';
+import SubscriptionCard from './_components/SubscriptionCard/SubscriptionCard';
+import { PLAN_MAPPING } from '@/constants/common';
 
 export default function Subscription() {
   const router = useRouter();
+  const { data: plans } = useGetPlans();
+
+  const reorderedPlans = plans ? [plans[plans.length - 1], ...plans.slice(0, plans.length - 1)] : [];
 
   const handleClick = () => {
     router.push('/payment');
@@ -16,33 +22,44 @@ export default function Subscription() {
 
   return (
     <S.Subscription>
-      <S.SelectPlan>
-        <S.TextWrapper>
-          <S.Heading>
-            {'‘나’'}를 알아가는 과정이
-            <br />
-            더욱 의미있도록
-          </S.Heading>
-          <S.Subtext>코그룸 구독으로 프리미엄 기능을 누려보세요</S.Subtext>
-        </S.TextWrapper>
+      <S.FirstLayout>
+        <S.SelectPlan>
+          <S.TextWrapper>
+            <S.Heading>
+              {'‘나’'}를 알아가는 과정이
+              <br />
+              더욱 의미있도록
+            </S.Heading>
+            <S.Subtext>코그룸 구독으로 프리미엄 기능을 누려보세요</S.Subtext>
+          </S.TextWrapper>
 
-        <Image
-          src='/SubscriptionCard.png'
-          alt='코그룸 프리미엄 구독 카드'
-          width={1117}
-          height={695}
-          priority
-        />
+          <S.SubscriptionCardWrapper>
+            {reorderedPlans.map((plan) => (
+              <SubscriptionCard
+                key={plan.planId}
+                id={plan.planId}
+                name={plan.name}
+                basePrice={plan.basePrice}
+                baseDiscountRate={plan.baseDiscountRate}
+                finalPrice={plan.finalPrice}
+                monthlyPrice={plan.monthlyPrice}
+                description={plan.description}
+                isBestValue={plan.planId === PLAN_MAPPING['YEAR']}
+                isSubscribed={plan.planId === PLAN_MAPPING['FREE']}
+              />
+            ))}
+          </S.SubscriptionCardWrapper>
 
-        <SolidButton
-          size='lg'
-          label='지금 시작하기'
-          color='primary'
-          interactionVariant='normal'
-          onClick={handleClick}
-          round
-        />
-      </S.SelectPlan>
+          <SolidButton
+            size='lg'
+            label='지금 시작하기'
+            color='primary'
+            interactionVariant='normal'
+            onClick={handleClick}
+            round
+          />
+        </S.SelectPlan>
+      </S.FirstLayout>
     </S.Subscription>
   );
 }
