0078ac0ebdd824bad78e77c1f88c60d31a7039a6
Leesugyoung <tnrud4685@naver.com>
2025-11-15 22:50:55 +0900

    feat: 세팅그룹 추가

--- src/app/(shared)/(standard)/mypage/purchase/subscribe/page.tsx
+++ src/app/(shared)/(standard)/mypage/purchase/subscribe/page.tsx
@@ -1,6 +1,22 @@
-import ScriptX from '@/assets/icons/script-x.svg';
-import EmptyState from '@/components/organisms/EmptyState/EmptyState';
+'use client';
+
+import useGetUserSubscription from '@/hooks/api/member/useGetUserSubscription';
+
+import { CurrentPlanBanner } from './_components/CurrentPlanBanner';
+import { PlanDescription } from './_components/PlanDescription';
+import SettingGroup from '../../notification/_components/SettingGroup/SettingGroup';
 
 export default function Subscribe() {
-  return <EmptyState icon={<ScriptX />} />;
+  const { data: subscription, error } = useGetUserSubscription();
+
+  return (
+    <>
+      <SettingGroup title='나의 플랜'>
+        <CurrentPlanBanner subscription={subscription ?? null} />
+      </SettingGroup>
+      <SettingGroup title='구독 관리'>
+        <PlanDescription subscription={subscription ?? null} />
+      </SettingGroup>
+    </>
+  );
 }
