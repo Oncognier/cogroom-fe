1b3ef9e6b658cd44f1654d93f4fa352511b4f458
Seio924 <seio9241@gmail.com>
2025-11-17 17:18:02 +0900

    feat: 구독페이지 구현

--- src/app/(shared)/(anomaly)/subscription/page.styled.ts
+++ src/app/(shared)/(anomaly)/subscription/page.styled.ts
@@ -7,11 +7,19 @@ export const Subscription = styled.div`
   flex-direction: column;
 `;
 
-export const FirstLayout = styled.div`
+export const SectionContainer = styled.div<{
+  isLast?: boolean;
+  isBlue?: boolean;
+  isWhite?: boolean;
+}>`
   padding: 12rem 0;
+  padding-bottom: ${({ isLast }) => isLast && '23.2rem'};
+
+  background-color: ${({ theme, isBlue, isWhite }) =>
+    isBlue ? theme.palette.blue[70] : isWhite ? theme.cogroom.white : 'transparent'};
 `;
 
-export const SelectPlan = styled.div`
+export const PlanSection = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -22,7 +30,7 @@ export const SelectPlan = styled.div`
   margin: 0 auto;
 `;
 
-export const TextWrapper = styled.div`
+export const SubscriptionText = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -48,4 +56,93 @@ export const SubscriptionCardWrapper = styled.div`
   gap: 3.2rem;
 
   width: 100%;
+`;
+
+export const FeatureSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 12rem;
+
+  max-width: 83.6rem;
+  margin: 0 auto;
+`;
+
+export const Feature = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4rem;
+
+  width: 100%;
+`;
+
+export const FeatureText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+
+  width: 100%;
+`;
+
+export const Title = styled.p`
+  padding: 1.8rem 3.2rem;
+  ${({ theme }) => theme.typography.heading2.semibold};
+  color: ${({ theme }) => theme.semantic.primary.normal};
+  background-color: ${({ theme }) => theme.semantic.background.elevated.normal};
+  border-radius: 999px;
+`;
+
+export const Description = styled.p`
+  ${({ theme }) => theme.typography.title2.bold};
+  color: ${({ theme }) => theme.cogroom.white};
+
+  text-align: center;
+`;
+
+export const UpgradeSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 6rem;
+
+  max-width: 86.8rem;
+  margin: 0 auto;
+`;
+
+export const UpgradeText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+
+  width: 100%;
+`;
+
+export const FreeTrialText = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 1.8rem;
+
+  ${({ theme }) => theme.typography.title1.bold};
+  color: ${({ theme }) => theme.semantic.label.normal};
+`;
+
+export const UpgradeTitle = styled.p`
+  ${({ theme }) => theme.typography.display1.medium};
+  color: ${({ theme }) => theme.semantic.label.normal};
+
+  strong {
+    ${({ theme }) => theme.typography.display1.bold};
+  }
+`;
+
+export const UpgradeDescription = styled.p`
+  ${({ theme }) => theme.typography.title3.regular};
+  color: ${({ theme }) => theme.semantic.label.normal};
+
+  text-align: center;
 `;
--- src/app/(shared)/(anomaly)/subscription/page.tsx
+++ src/app/(shared)/(anomaly)/subscription/page.tsx
@@ -1,9 +1,17 @@
 'use client';
 
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 import SolidButton from '@/components/atoms/SolidButton/SolidButton';
+import Thumbnail from '@/components/atoms/Thumbnail/Thumbnail';
 import { PLAN_MAPPING } from '@/constants/common';
+import {
+  DEFAULT_FEATURE_BANNER_1,
+  DEFAULT_FEATURE_BANNER_2,
+  DEFAULT_FEATURE_BANNER_3,
+  DEFAULT_UPGRADE_BANNER,
+} from '@/constants/image';
 import useGetUserSummary from '@/hooks/api/member/useGetUserSummary';
 import { useGetPlans } from '@/hooks/api/payment/useGetPlans';
 
@@ -17,11 +25,15 @@ export default function Subscription() {
 
   const reorderedPlans = plans ? [plans[plans.length - 1], ...plans.slice(0, plans.length - 1)] : [];
 
+  const handleFreeTrial = () => {
+    router.push('/payment?plan=MONTH&trial=true');
+  };
+
   const handleClick = () => {
     if (userSummary?.isTrialUsed) {
       router.push('/payment');
     } else {
-      router.push('/payment?plan=MONTH&trial=true');
+      handleFreeTrial();
     }
   };
 
@@ -29,16 +41,16 @@ export default function Subscription() {
 
   return (
     <S.Subscription>
-      <S.FirstLayout>
-        <S.SelectPlan>
-          <S.TextWrapper>
+      <S.SectionContainer>
+        <S.PlanSection>
+          <S.SubscriptionText>
             <S.Heading>
               {'‘나’'}를 알아가는 과정이
               <br />
               더욱 의미있도록
             </S.Heading>
             <S.Subtext>코그룸 구독으로 프리미엄 기능을 누려보세요</S.Subtext>
-          </S.TextWrapper>
+          </S.SubscriptionText>
 
           <S.SubscriptionCardWrapper>
             {reorderedPlans.map((plan) => (
@@ -65,8 +77,75 @@ export default function Subscription() {
             onClick={handleClick}
             round
           />
-        </S.SelectPlan>
-      </S.FirstLayout>
+        </S.PlanSection>
+      </S.SectionContainer>
+      <S.SectionContainer isBlue>
+        <S.FeatureSection>
+          <S.Feature>
+            <S.FeatureText>
+              <S.Title>스트릭 부활권</S.Title>
+              <S.Description>실수해도 데일리 스트릭을 잃지 않아요!</S.Description>
+            </S.FeatureText>
+            <Thumbnail
+              src={DEFAULT_FEATURE_BANNER_1}
+              ratio='16_9'
+            />
+          </S.Feature>
+          <S.Feature>
+            <S.FeatureText>
+              <S.Title>답변 수 100자 → 500자</S.Title>
+              <S.Description>길이에 구애받지 않고 마음껏 생각을 이어가 보세요</S.Description>
+            </S.FeatureText>
+            <Thumbnail
+              src={DEFAULT_FEATURE_BANNER_2}
+              ratio='16_9'
+            />
+          </S.Feature>
+          <S.Feature>
+            <S.FeatureText>
+              <S.Title>무한 수정 & 무한 공유</S.Title>
+              <S.Description>지난 데일리 사유를 돌아보고 수정할 수 있어요</S.Description>
+              <S.Description>자유롭게 커뮤니티에 어제, 오늘, 지난날의 인사이트 공유해요</S.Description>
+            </S.FeatureText>
+            <Thumbnail
+              src={DEFAULT_FEATURE_BANNER_3}
+              ratio='16_9'
+              radius
+            />
+          </S.Feature>
+        </S.FeatureSection>
+      </S.SectionContainer>
+      <S.SectionContainer
+        isWhite
+        isLast
+      >
+        <S.UpgradeSection>
+          <S.UpgradeText>
+            <S.UpgradeDescription>&apos;나&apos;를 알아가는 여정이 더욱 의미있도록</S.UpgradeDescription>
+            <S.UpgradeTitle>
+              지금, 책 1/2 권 값으로 <strong>업그레이드</strong> 하기
+            </S.UpgradeTitle>
+          </S.UpgradeText>
+          <S.FreeTrialText>
+            또는, 여기서
+            <SolidButton
+              size='lg'
+              color='primary'
+              label='무료 체험'
+              interactionVariant='normal'
+              onClick={handleFreeTrial}
+              round
+            />
+            시작!
+          </S.FreeTrialText>
+          <Image
+            src={DEFAULT_UPGRADE_BANNER}
+            alt='업그레이드 안내 배너'
+            width={868}
+            height={944}
+          />
+        </S.UpgradeSection>
+      </S.SectionContainer>
     </S.Subscription>
   );
 }
