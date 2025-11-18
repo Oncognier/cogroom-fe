b950232981439da2effb05dad876f620033bb434
Leesugyoung <tnrud4685@naver.com>
2025-11-18 18:03:32 +0900

    feat: 마이페이지 쿠폰 탭 및 등록기능 추가

---
+++ src/app/(shared)/(standard)/mypage/purchase/coupons/page.styled.ts
@@ -0,0 +1,15 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const CouponForm = styled.form`
+  display: flex;
+  flex-direction: row;
+  gap: 2.4rem;
+
+  width: 100%;
+
+  & > div:first-of-type {
+    flex: 1;
+  }
+`;
