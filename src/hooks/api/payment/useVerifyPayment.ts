2266387c224932b8d55b7689b4429948afa455cb
Seio924 <seio9241@gmail.com>
2025-11-28 18:35:35 +0900

    refactor: 결제 검증 훅에서 빌링키 생성 책임 분리

--- src/hooks/api/payment/useVerifyPayment.ts
+++ src/hooks/api/payment/useVerifyPayment.ts
@@ -1,27 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { paymentApi } from '@/api/paymentApis';
-import { requestIssueBillingKey } from '@/utils/portone';
 
 export const useVerifyPaymentMutation = () => {
   const mutation = useMutation({
     mutationFn: paymentApi.verifyPayment,
-    onSuccess: async ({ email, phoneNumber, name, paymentHistoryId, finalPrice, planName }) => {
-      await requestIssueBillingKey({
-        finalPrice,
-        paymentHistoryId,
-        planName,
-        customer: {
-          fullName: name,
-          phoneNumber: phoneNumber,
-          email: email,
-        },
-      });
-    },
     onError: () => {
       alert('결제에 실패하였습니다');
     },
   });
 
-  return { verifyPayment: mutation.mutate };
+  return { verifyPayment: mutation.mutateAsync };
 };
