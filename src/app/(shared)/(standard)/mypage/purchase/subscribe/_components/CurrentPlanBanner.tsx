873a7302c0e1155e3400a0973c9a613ebb2c5d4a
Leesugyoung <tnrud4685@naver.com>
2025-12-05 22:45:38 +0900

    refactor: UI 기획 변경으로 인한 버튼삭제

--- src/app/(shared)/(standard)/mypage/purchase/subscribe/_components/CurrentPlanBanner.tsx
+++ src/app/(shared)/(standard)/mypage/purchase/subscribe/_components/CurrentPlanBanner.tsx
@@ -13,33 +13,11 @@ export const CurrentPlanBanner = ({ subscription }: CurrentPlanBannerProps) => {
   const isFreePlan = planId === 3;
   const planName = isFreePlan ? '무료플랜' : subscription?.name || '무료플랜';
 
-  const goPlan = () => {};
-
-  const isPremium = planId === 2;
-
   return (
     <S.PlanBannerContainer>
       <S.PlanName>
         현재 코그룸 <S.CurrentPlanName>{planName}</S.CurrentPlanName>을 이용하고 있어요.
       </S.PlanName>
-
-      {isPremium ? (
-        <OutlinedButton
-          size='sm'
-          label='현재 혜택을 최대한 누리고 있어요'
-          color='primary'
-          interactionVariant='normal'
-          onClick={goPlan}
-        />
-      ) : (
-        <SolidButton
-          size='sm'
-          label='업그레이드 하고 더욱 깊이 성장하기'
-          color='primary'
-          interactionVariant='normal'
-          onClick={goPlan}
-        />
-      )}
     </S.PlanBannerContainer>
   );
 };
